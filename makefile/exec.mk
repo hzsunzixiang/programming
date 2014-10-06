09c29048949714dd731e24ba4e1546de8b54bb96
root <root@debian>
2014-10-06 13:37:20 -0700

    makefile

---
+++ makefile/exec.mk
@@ -0,0 +1,15 @@
+exec: 
+	cd /tmp; pwd 
+
+#	root@debian:~/programming/makefile# make -f exec.mk exec
+#	cd /tmp; pwd
+#	/tmp
+
+
+#When it is time to execute recipes to update a target, they are executed by invoking a new sub-shell for each line of the recipe, unless the .ONESHELL special target is in effect (see Using One Shell) (In practice, make may take shortcuts that do not affect the results.)
+#
+#		Please note: this implies that setting shell variables and invoking shell commands such as cd that set a context local to each process will not affect the following lines in the recipe.2 If you want to use cd to affect the next statement, put both statements in a single recipe line. Then make will invoke one shell to run the entire line, and the shell will execute the statements in sequence. For example:
+#
+#		foo : bar/lose
+#		        cd $(@D) && gobble $(@F) > ../$@
+#				Here we use the shell AND operator (&&) so that if the cd command fails, the script will fail without trying to invoke the gobble command in the wrong directory, which could cause problems (in this case it would certainly cause ../foo to be truncated, at least).
