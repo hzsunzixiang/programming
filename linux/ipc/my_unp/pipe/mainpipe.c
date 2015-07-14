e106a2ba77e3d83034b6a7cff3a829f40a5ad3a8
root <root@localhost>
2015-07-14 22:25:48 +0800

    pipe client and server

--- linux/ipc/my_unp/pipe/mainpipe.c
+++ linux/ipc/my_unp/pipe/mainpipe.c
@@ -3,6 +3,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 
 /*
  * The  following program creates a pipe, and then fork(2)s to create a child process; the child inherits a duplicate set of file descriptors that refer to the same pipe.  After the
@@ -18,10 +23,6 @@ main(int argc, char *argv[])
 	int pipe1[2], pipe2[2];;
 	pid_t childpid;
 
-	if (argc != 2) {
-		fprintf(stderr, "Usage: %s <string>\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
 
 	if (pipe(pipe1) == -1) {
 		perror("pipe1");
@@ -38,7 +39,8 @@ main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	if (childpid== 0) {    /* Child reads from pipe */
+	if (childpid == 0) {    /* Child reads from pipe */
+		fprintf(stderr, "child......\n");
 		close(pipe1[1]);          /* Close unused write end */
 		close(pipe2[0]);          /* Close unused read end */
 
@@ -67,11 +69,111 @@ void client(int readfd, int writefd)
 	// A newline character makes fgets stop reading, but it is considered a valid character by the function and included in the string copied to str.
 	//
 	// A terminating null character is automatically appended after the characters copied to str.
-	char * ret = fgets(buff, MAXLINE, stdin);
+	//char * ret = fgets(buff, MAXLINE, stdin);
+	fgets(buff, MAXLINE, stdin);
 	len = strlen(buff);
-	
+	if (buff[len-1] == '\n')
+	{
+		len--;
+	}
+	// write pathname to ipc channel
+	//// 实验两次写完
+	//if (write(writefd, buff, 2) < 0)
+	//{
+	//	perror("write to pipe:");
+	//	exit(EXIT_FAILURE);
+	//}
+	//sleep(1);
+	//if (write(writefd, buff + 2, len - 2) < 0)
+	//{
+	//	perror("write to pipe:");
+	//	exit(EXIT_FAILURE);
+	//}
+	if (write(writefd, buff, len) < 0)
+	{
+		perror("write to pipe:");
+		exit(EXIT_FAILURE);
+	}
+	// read from ipc , write to standard output
+	while (1)
+	{
+		n = read(readfd, buff, MAXLINE);
+		if (n == 0)
+		{
+			break;
+		}
+		else if(n > 0) 
+		{
+			if (write(STDOUT_FILENO, buff, n) < 0)
+			{
+				perror("write to stdout:");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			perror("read from pipe:");
+			exit(EXIT_FAILURE);
+		}
+	}
 }
 
 void server(int readfd, int writefd)
 {
+	int fd;
+	ssize_t n;
+	char buff[MAXLINE + 1];
+
+	n = read(readfd, buff, MAXLINE);
+	if (n == 0)
+	{
+		perror("end of file while reading pathname");
+	}
+	else if(n > 0) 
+	{
+		buff[n] = '\0';
+	}
+	else
+	{
+		perror("read from pipe:");
+		exit(EXIT_FAILURE);
+	}
+	if ((fd = open(buff, O_RDONLY)) < 0)
+	{
+		fprintf(stderr, "buff=%s\n", buff);
+		snprintf(buff + n, sizeof(buff) - n, "can't open , %s\n", strerror(errno));
+		n = strlen(buff);
+		if (write(writefd, buff, n) < 0)
+		{
+			perror("write to pipe:");
+			exit(EXIT_FAILURE);
+		}
+
+	}
+	else
+	{
+		while (1)
+		{
+			n = read(fd, buff, MAXLINE);
+			if (n == 0)
+			{
+				break;
+			}
+			else if(n > 0) 
+			{
+				if (write(writefd, buff, n) < 0)
+				{
+					perror("write to stdout:");
+					exit(EXIT_FAILURE);
+				}
+			}
+			else
+			{
+				perror("read from pipe:");
+				exit(EXIT_FAILURE);
+			}
+		}
+		close(fd);
+	}
+
 }
