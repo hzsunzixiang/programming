a82ff68d559a1f79e291f8cb9f3347ef09fea8ea
hzsunzixiang <haichengsun123@163.com>
2015-07-16 20:25:05 +0800

    fifo iterate server : Dos attach

---
+++ linux/ipc/my_unp/pipe/fifocliserv/client_main.c
@@ -0,0 +1,85 @@
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<errno.h>
+#include<fcntl.h>
+
+#define MAXLINE 4096
+#define SERV_FIFO       "/tmp/fifo.serv"
+	int
+main(int argc, char *argv[])
+{
+	int readfifo, writefifo;
+	char *ptr, buff[MAXLINE + 1], fifoname[MAXLINE];
+	pid_t pid;
+	ssize_t n;
+	size_t len;
+
+	// create  FIFO with our PID as part of name
+	pid = getpid();
+	snprintf(fifoname, sizeof(fifoname), "/tmp/fifo.%ld", (long)pid);
+	printf("fifoname:%s\n", fifoname);
+	if ((mkfifo(fifoname, 0666) < 0) && (errno != EEXIST))
+	{
+		perror("mkfifo error");
+		exit(EXIT_FAILURE);
+	}
+
+	// start buff with pid and a blank
+	snprintf(buff, sizeof(buff), "%ld ", (long)pid);
+	len = strlen(buff);
+	ptr = buff + len;
+	fgets(ptr, MAXLINE-len, stdin);
+	len = strlen(buff);
+
+	//open fifo to server and write PID and pathname to FIFO
+	// 服务器的open函数调用 open(SERV_FIFO, O_RDONLY, 0);
+	// 将阻塞到第一个客户只写打开服务器的FIFO为止
+	//getchar();
+	writefifo = open(SERV_FIFO, O_WRONLY, 0);
+	//getchar();
+
+	printf("buff sent to server:%s  data:%s\n",SERV_FIFO, buff);
+	if ( (write(writefifo, buff, len)) < 0)
+	{
+		perror("write to fifo");
+		close(writefifo);
+	}
+
+	// now open our FIFO; blocks until server opens for writing
+	// 服务器端的 写FIFO 阻塞直到 ( (writefifo = open(fifoname, O_WRONLY, 0)) < 0)
+	// 所以恶意客户给他发送一个命令行，但从来不代开自己的FIFO来读，所以服务器端会一直阻塞，而不能服务下一个客户
+	//getchar(); // 打开这一行，可以模拟Dos攻击 ,在这里停下，导致其他客户端无法响应请求
+	readfifo = open(fifoname, O_RDONLY, 0);
+	//getchar();
+	printf("read from fifoname :%s\n", fifoname);
+	while(1){
+		n = read(readfifo, buff, MAXLINE);
+		if (n < 0)
+		{
+			perror("read  server pipe error");
+			return EXIT_FAILURE;
+		}
+		else if(n ==0)
+		{
+			break;
+		}
+		printf("read from fifoname :%s, n = %ld\n", fifoname, n);
+		if ( (write(STDOUT_FILENO, buff, n)) < 0)
+		{
+			perror("write to fifo");
+			close(writefifo);
+		}
+	}
+	close(readfifo);
+	unlink(fifoname);
+	exit(EXIT_SUCCESS);
+}
+
+
+
---
+++ linux/ipc/my_unp/pipe/fifocliserv/server_main.c
@@ -0,0 +1,158 @@
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<errno.h>
+#include<fcntl.h>
+
+#define SERV_FIFO       "/tmp/fifo.serv"
+
+#define MAXLINE 4096
+ssize_t
+readline(int fd, void *vptr, size_t maxlen);
+	int
+main(int argc, char *argv[])
+{
+	int readfifo, writefifo, dummyfd, fd; 
+	char *ptr, buff[MAXLINE + 1], fifoname[MAXLINE];
+	pid_t pid;
+	ssize_t n;
+
+	// create server's well-known FIFO; OK if they already exist
+	//
+	if ((mkfifo(SERV_FIFO, 0666) < 0) && (errno != EEXIST))
+	{
+		perror("mkfifo SERV_FIFO");
+		exit(EXIT_FAILURE);
+	}
+	// open server's well-known FIFO for reading and writing 
+
+	readfifo = open(SERV_FIFO, O_RDONLY, 0);
+	printf("open  %s for read  return\n", SERV_FIFO);
+
+	writefifo = open(SERV_FIFO, O_WRONLY, 0);
+	printf("open  %s for read and write \n", SERV_FIFO);
+	while(1){
+		n = readline(readfifo, buff, MAXLINE);
+		if (n < 0)
+		{
+			perror("readline error");
+			return EXIT_FAILURE;
+		}
+		else if(n ==0)
+		{
+			break;
+		}
+		if (buff[n-1] =='\n')
+		{
+			n--;
+		}
+		buff[n] = '\0';
+		printf("read from client %s\n", buff);
+		if ((ptr = strchr(buff, ' ')) ==NULL)
+		{
+			perror("bogus, request : ");
+			continue;
+		}
+		*ptr++ = 0;
+		pid = atol(buff);
+		snprintf(fifoname, sizeof(fifoname), "/tmp/fifo.%ld", (long)pid);
+		if ( (writefifo = open(fifoname, O_WRONLY, 0)) < 0)
+		{
+			perror("cannot open ");
+			continue;
+		}
+		printf("open %s return\n", fifoname);
+		if ( (fd = open(ptr, O_RDONLY)) < 0)
+		{
+			// error, mush tell client
+			snprintf(buff + n, sizeof(buff) - n, ":cannot open ,%s\n", strerror(errno) );
+			n = strlen(ptr);
+			if ( (write(writefifo, ptr, n)) < 0)
+			{
+				perror("write to fifo");
+				close(writefifo);
+			}
+		}
+		else
+		{
+			// open succeeded : copy file to FIFO
+			n = read(fd, buff, MAXLINE);
+			if (n < 0)
+			{
+				perror("read file  error");
+				return EXIT_FAILURE;
+			}
+			else if(n ==0)
+			{
+				break;
+			}
+
+			if ( (write(writefifo, buff, n)) < 0)
+			{
+				perror("write to fifo");
+				close(writefifo);
+			}
+
+			close(fd);
+			close(writefifo);
+		}
+	}
+	exit(EXIT_SUCCESS);
+}
+
+
+
+	static ssize_t
+my_read(int fd, char *ptr)
+{
+	static int	read_cnt = 0;
+	static char	*read_ptr;
+	static char	read_buf[MAXLINE];
+
+	if (read_cnt <= 0) {
+again:
+		if ( (read_cnt = read(fd, read_buf, sizeof(read_buf))) < 0) {
+			if (errno == EINTR)
+				goto again;
+			return(-1);
+		} else if (read_cnt == 0)
+			return(0);
+		read_ptr = read_buf;
+	}
+
+	read_cnt--;
+	*ptr = *read_ptr++;
+	return(1);
+}
+
+	ssize_t
+readline(int fd, void *vptr, size_t maxlen)
+{
+	int		n, rc;
+	char	c, *ptr;
+
+	ptr = vptr;
+	for (n = 1; n < maxlen; n++) {
+		if ( (rc = my_read(fd, &c)) == 1) {
+			*ptr++ = c;
+			if (c == '\n')
+				break;	/* newline is stored, like fgets() */
+		} else if (rc == 0) {
+			if (n == 1)
+				return(0);	/* EOF, no data read */
+			else
+				break;		/* EOF, some data was read */
+		} else
+			return(-1);		/* error, errno set by read() */
+	}
+
+	*ptr = 0;	/* null terminate like fgets() */
+	return(n);
+}
+/* end readline */
+
