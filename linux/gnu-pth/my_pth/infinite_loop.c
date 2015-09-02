58d530f439a1a358434db20abf6689262e8c52c0
hzsunzixiang <haichengsun123@163.com>
2015-09-02 13:59:12 +0800

    the interator for setcontext in wiki

--- linux/gnu-pth/my_pth/infinite_loop.c
+++ linux/gnu-pth/my_pth/infinite_loop.c
@@ -18,3 +18,30 @@ int main(int argc, const char *argv[]){
 //  makecontext(3), or passed as third argument to a signal handler.
 //  If the context was obtained by a call of getcontext(), program execution continues as if this call just returned.
 
+
+
+//    (gdb) info b
+//    Num     Type           Disp Enb Address            What
+//    2       breakpoint     keep y   0x0000000000400628 in main at infinite_loop.c:9
+//            breakpoint already hit 1 time
+//    3       breakpoint     keep y   0x000000000040064b in main at infinite_loop.c:12
+//            breakpoint already hit 3 times
+//    5       breakpoint     keep y   0x0000000000400641 in main at infinite_loop.c:11
+//            breakpoint already hit 1 time
+//    (gdb) p/x $rip
+//    $20 = 0x400641
+//    (gdb) n
+//    
+//    Breakpoint 3, main (argc=1, argv=0x7fffffffe428) at infinite_loop.c:12
+//    12              setcontext(&context);
+//    (gdb) p/x $rip
+//    $21 = 0x40064b
+//    (gdb) n
+//    Hello world
+//    
+//    Breakpoint 5, main (argc=1, argv=0x7fffffffe428) at infinite_loop.c:11
+//    11              sleep(1);
+//    (gdb) p/x $rip
+//    $22 = 0x400641
+//    (gdb)
+//    
