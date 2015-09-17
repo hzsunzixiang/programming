65317ce8e7eaa438ecaea04271ada902832bcf03
hzsunzixiang <haichengsun123@163.com>
2015-09-17 16:32:28 +0800

    abstract class and class dependent

---
+++ cpp/abstractbaseclass/classDepend.cpp
@@ -0,0 +1,50 @@
+
+
+#include<stdio.h>
+
+// 这里可以构造一个无限循环
+//
+// 必须声明一个虚基类才能达到目的，不然在A中不可以直接调用 b->printB()
+
+// 一直到coredump为止
+class B;
+
+// Bx 在这里仅仅是一个接口
+class Bx {
+	public:
+		virtual void printB()=0;
+		
+};
+class A {
+	public:
+		void printA()
+		{
+			printf("in A\n");
+			b->printB();    //  如果只是前向声明，B达不到目的
+		}
+	public:
+		Bx * b;
+		
+};
+
+class B:public Bx {
+	public:
+		void printB()
+		{
+			printf("in B\n");
+			a->printA();
+		}
+	public:
+		A* a;
+};
+
+int main()
+{
+ 	A * a1 = new A();
+	B * b1 = new B();
+	b1->a = a1;
+	a1->b = b1;
+	b1->printB();
+
+	return 0;
+}
