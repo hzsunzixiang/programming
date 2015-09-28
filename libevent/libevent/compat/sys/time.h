a29d36e0824bd0fc630da5291913343c8c5c7ab5
hzsunzixiang <haichengsun123@163.com>
2015-09-28 20:25:10 +0800

    libevent-0.6 merely changed

--- libevent/libevent/compat/sys/time.h
+++ libevent/libevent/compat/sys/time.h
@@ -41,18 +41,18 @@
 
 #include <sys/types.h>
 
-/*
- * Structure returned by gettimeofday(2) system call,
- * and used in other calls.
- */
+///*
+// * Structure returned by gettimeofday(2) system call,
+// * and used in other calls.
+// */
 //struct timeval {
 //	long	tv_sec;		/* seconds */
 //	long	tv_usec;	/* and microseconds */
 //};
-
-/*
- * Structure defined by POSIX.1b to be like a timeval.
- */
+//
+///*
+// * Structure defined by POSIX.1b to be like a timeval.
+// */
 //struct timespec {
 //	time_t	tv_sec;		/* seconds */
 //	long	tv_nsec;	/* and nanoseconds */
