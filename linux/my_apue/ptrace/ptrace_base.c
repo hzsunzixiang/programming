f03209bfddf0ed66796d3503a4bf31839b2d78a8
hzsunzixiang <haichengsun123@163.com>
2015-10-09 20:31:06 +0800

    ptrace register and tencent libco

--- linux/my_apue/ptrace/ptrace_base.c
+++ linux/my_apue/ptrace/ptrace_base.c
@@ -5,23 +5,26 @@
 #include <unistd.h>  
 #include <sys/user.h>   /* For constants   ORIG_EAX etc */  
 #include <sys/reg.h>   /* For constants   ORIG_EAX etc */  
+
+
 int main()  
 {  
 	pid_t child;  
 	long orig_eax;  
+
 	child = fork();  
 	if(child == 0) {  
 		ptrace(PTRACE_TRACEME, 0, NULL, NULL);  
-		execl("/bin/ls", "ls", NULL);  
+		printf("after ptrace but, not system call, sleep\n");
+		sleep(5); // 在没调用系统之前， wait不返回 
+		execl("/bin/ls", "ls", NULL);    // /usr/include/i386-linux-gnu/asm/unistd_32.h #define __NR_execve 11
 	}  
 	else {  
 		wait(NULL);  
-		orig_eax = ptrace(PTRACE_PEEKUSER,   
-				child, 4 * ORIG_EAX,   
-				NULL);  
-		printf("The child made a "  
-				"system call %ld\n ", orig_eax);  
-		sleep(10);
+		printf("after return from wait\n");
+		orig_eax = ptrace(PTRACE_PEEKUSER, child, 4 * ORIG_EAX, NULL);  
+		printf("The child made a system call %ld\n ", orig_eax);  
+		sleep(10); // 返回之后，子进程继续执行ls
 		ptrace(PTRACE_CONT, child, NULL, NULL);  
 	}  
 	return 0;  
