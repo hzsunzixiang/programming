a8245557fe44bcac61d802a46699dcf66dd4b39b
hzsunzixiang <haichengsun123@163.com>
2015-11-23 18:18:11 +0800

    reverse link

---
+++ datastructure/c_ds/myds/reverse_link/reverse.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+typedef struct Node {
+	char data;
+	struct Node* next;
+} Node;
+
+void print_list(Node* root) {
+	while (root) {
+		printf("%c ", root->data);
+		root = root->next;
+	}
+	printf("\n");
+}
+
+Node* reverse(Node* root) {
+	Node* new_root = 0;
+	while (root) {
+		Node* next = root->next;
+		root->next = new_root;
+		new_root = root;
+		root = next;
+	}
+	return new_root;
+}
+
+int main() {
+	Node d = { 'd', 0 };
+	Node c = { 'c', &d };
+	Node b = { 'b', &c };
+	Node a = { 'a', &b };
+
+	Node* root = &a;
+	print_list(root);
+	root = reverse(root);
+	print_list(root);
+
+	return 0;
+}
+// http://www.geeksforgeeks.org/write-a-function-to-reverse-the-nodes-of-a-linked-list/
+/* Function to reverse the linked list */
+// static void reverse(struct node** head_ref)
+// {
+// 	struct node* prev   = NULL;
+// 	struct node* current = *head_ref;
+// 	struct node* next;
+// 	while (current != NULL)
+// 	{
+// 		next  = current->next;  
+// 		current->next = prev;   
+// 		prev = current;
+// 		current = next;
+// 	}
+// 	*head_ref = prev;
+// }
+
