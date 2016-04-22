223981d3d4baa4dd8974876306804951e946f7c5
hzsunzixiang <haichengsun123@163.com>
2016-04-22 14:51:04 +0800

    memcpy  advanced

--- linux/memcpy/memcpy_htons.c
+++ linux/memcpy/memcpy_htons.c
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <arpa/inet.h>
+
 #define  PACKET_BUFFER_SIZE 100
 
 #define  PACKET_HEADER_SIZE 13 
@@ -24,13 +26,34 @@ void _writeHeader(char *pIn, int nLen, int nPos)
 		memcpy(m_strBuf+nPos, pIn, nLen) ;
 	}
 }
-void _begin(short nCmdType, char cVersion, char cSubVersion)
+void _begin1(short nCmdType, char cVersion, char cSubVersion)
 {
 	_reset();
+	//short tmp = htons(nCmdType);
+	//_writeHeader((char*)&tmp, sizeof(short), offset);// 命令码
 	_writeHeader("IC", sizeof(char)*2, 0);// 命令码
 	_writeHeader((char*)&nCmdType, sizeof(short), 2);// 命令码
 	_writeHeader(&cVersion, sizeof(char), 4);		// 主版本号
 	_writeHeader(&cSubVersion, sizeof(char), 5);	// 子版本号
+}
+//	_begin(600, 15, 16, 1000, 0x1234567890);
+void _begin(short nCmdType, char cVersion, char cSubVersion, int extent, int64_t ext2)
+{
+	_reset();
+	size_t offset = 0;
+	_writeHeader("IC", sizeof(char)*2, offset);// 命令码
+	offset += 2; 
+	// 转换为网络字节序
+	short nCmdType_net = htons(nCmdType);
+	_writeHeader((char*)&nCmdType_net, sizeof(short), offset);// 命令码
+	offset += 2; 
+	_writeHeader(&cVersion, sizeof(char), offset);		// 主版本号
+	offset += 1; 
+	_writeHeader(&cSubVersion, sizeof(char), offset);	
+	offset += 1; 
+	_writeHeader((char *)&extent, sizeof(int), offset);	
+	offset += sizeof(int); 
+	_writeHeader((char *)&ext2, sizeof(int64_t), offset);	
 }
 
 void _readHeader(char *pOut, int nLen, int nPos)
@@ -66,37 +89,28 @@ short GetBodyLength(void)
 }
 int main()
 {
-	//_begin(100, 31, 7);
-	_begin(64, 15, 15);
+	_begin(0x56, 0x15, 0x16, 0x1000, 0x1234567890);
 	char version =  GetVersion();
 	printf("version:%d\n\n", version);
-	FILE* f = fopen("test.bin", "w+");
+	FILE* f = fopen("test1.bin", "w+");
 	fwrite(&m_strBuf, sizeof(m_strBuf), 1, f);
 
 	return 0;
 }
 
+// 0x0056 被转换为大端字节的时候
 
-
-
-// StephenSun@192.168.202.90:~/program$ hexdump test.bin
-// 0000000 4349 0001 0101 0000 0000 0000 0000 0000 //  _begin(1, 1, 1) 的结果
-// 0000010 0000 0000 0000 0000 0000 0000 0000 0000
+// stephensun@debian:~/programming/linux/memcpy$ hexdump -C test1.bin
+// 00000000  49 43 00 56 15 16 00 10  00 00 90 78 56 34 12 00  |IC.V.......xV4..|
+// 00000010  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00  |................|
 // *
-// 0000060 0000 0000
-// 0000064
+// 00000064
 
-// StephenSun@192.168.202.90:~/program$ ll test.bin
-// -rw-rw-r-- 1 StephenSun devel 100 Jan 24 19:39 test.bin   ----  注意大小是100字节
-
-
-// StephenSun@192.168.202.90:~/program$ hexdump test.bin // _begin(64, 15, 15);
-// 0000000 4349 0040 0f0f 0000 0000 0000 0000 0000
-// 0000010 0000 0000 0000 0000 0000 0000 0000 0000
-// *
-// 0000060 0000 0000
-// 0000064
-
-
-
+// (gdb) x/2xb &nCmdType
+// 0x7fffffffdfbc: 0x56    0x00
+// (gdb) x/2xb &nCmdType_net
+// 0x7fffffffdfc6: 0x00    0x56
+// (gdb) x/16xb m_strBuf
+// 0x600f80 <m_strBuf>:    0x49    0x43    0x00    0x56    0x00    0x00    0x00    0x00
+// 0x600f88 <m_strBuf+8>:  0x00    0x00    0x00    0x00    0x00    0x00    0x00    0x00
 
