2b21c83eb19446224a87023d78296720b8051d2f
root <root@debian>
2016-08-02 04:20:13 +0800

    automake helloworld

---
+++ linux/automake/hellotutorial/src/hw/cli.h
@@ -0,0 +1,11 @@
+/* @(#)cli.h
+ */
+
+#ifndef _CLI_H
+#define _CLI_H 1
+
+int parse_float_array(int argc, char **argv, double **result);
+
+
+#endif /* _CLI_H */
+
---
+++ linux/automake/hellotutorial/src/libmath/hwmath.h
@@ -0,0 +1,10 @@
+/* @(#)hwmath.h
+ */
+
+#ifndef _HWMATH_H
+#define _HWMATH_H 1
+
+double average(int nsize, double *numbers);
+
+#endif /* _HWMATH_H */
+
