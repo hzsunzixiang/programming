9f023e75e4d87034d9c3a1daa0043ea7aca07808
Your Name <you@example.com>
2017-07-31 17:59:51 +0800

    tshark 脚本

--- linux/tshark/run.sh
+++ linux/tshark/run.sh
@@ -1,2 +1,3 @@
 #sudo tshark -t a -r download.pcap -qz io,stat,1| awk -F'|' '$0 !~ /Time/' '{if(NF==5) print $0}'
-sudo tshark -t a -r download.pcap -qz io,stat,1| awk -F'|' '$2 !~ /Time/  {if(NF==5) print $0}' > xx.txt
+sudo tshark -t a -r tcpdump/download.pcap -qz io,stat,1| awk -F'|' '$2 !~ /Time/  {if(NF==5) print $0}' |awk -F "|" '{print $2,$3,$4}'
+#sudo tshark -t a -r download.pcap -qz io,stat,1| awk -F'|' '$2 !~ /Time/  {if(NF==5) print $0}' > xx.txt
