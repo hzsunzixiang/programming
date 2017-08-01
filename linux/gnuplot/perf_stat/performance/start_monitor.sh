f8a0d8bbe101c5ae386a8c6f8bfc2843a30b2913
Your Name <you@example.com>
2017-08-01 12:17:28 +0800

    statistics

--- linux/gnuplot/perf_stat/performance/start_monitor.sh
+++ linux/gnuplot/perf_stat/performance/start_monitor.sh
@@ -3,7 +3,7 @@
 #-o或--options选项后面接可接受的短选项，如ab:c::，表示可接受的短选项为-a -b -c，其中-a选项不接参数，-b选项后必须接参数，-c选项的参数为可选的
 #-l或--long选项后面接可接受的长选项，用逗号分开，冒号的意义同短选项。
 #-n选项后接选项解析错误时提示的脚本名字
-ARGS=`getopt -o hd:i:p:f: --long duration:,pid:,file:,help -n 'example.sh' --  "$@"`
+ARGS=`getopt -o hd:i:p:f:P: --long duration:,pid:,file:,port:,help -n 'example.sh' --  "$@"`
 if [ $? != 0 ]; then
     echo "Terminating..."
     exit 1
@@ -11,7 +11,7 @@ fi
 
 usage ()
 {
-	echo "usage: $0 -d <duaration> -p <pid> -f <directory>" 
+	echo "usage: $0 -d <duaration> -p <pid> -P <PORT> -f <directory>" 
 	exit
 }
 
@@ -30,6 +30,11 @@ do
         -p|--pid)
             echo "Option p, argument $2";
 			export PROCESSID=$2
+            shift 2
+            ;;
+        -P|--port)
+            echo "Option P, argument $2";
+			export PORT=$2
             shift 2
             ;;
         -f|--file)
@@ -62,18 +67,18 @@ then
 	export DIRECTORY="logs"
 fi
 
+if test -z "$PORT"
+then 
+	echo "port is null exit"
+	exit 1
+fi
+
 OUTPUT_DIR="$DIRECTORY"
 mkdir -p $OUTPUT_DIR
 #rm -f $OUTPUT_DIR/*
 
-
-
-
-/usr/bin/env top -p $PROCESSID -b -d$DURATION |grep -w  $PROCESSID -B1 --line-buffered| awk '/[^--]/{
-	if($0 ~ /PID/) 
-		print "CUR_TIME "$0;
-	else
-		print strftime("%T",systime()), $0;
-	fflush()
-	}' >>$OUTPUT_DIR/top_data.out &
-
+pidstat -d -p $PROCESSID  $DURATION  > $OUTPUT_DIR/diskio_data.txt  &
+pidstat -r -p $PROCESSID  $DURATION  > $OUTPUT_DIR/memory_data.txt  & 
+pidstat -u -p $PROCESSID  $DURATION  > $OUTPUT_DIR/cpu_data.txt     &
+pidstat -w -p $PROCESSID  $DURATION  > $OUTPUT_DIR/switch_data.txt  &
+sudo tcpdump -pnni any port $PORT -Uw   $OUTPUT_DIR/network_data_origin.pcap&
--- linux/gnuplot/perf_stat/performance/stop_monitor.sh
+++ linux/gnuplot/perf_stat/performance/stop_monitor.sh
@@ -4,4 +4,6 @@
 #ps ux | grep 'iostat -d -xm' | grep -v grep | awk '{print $2}' | xargs kill
 #ps ux | grep 'iostat -d' | grep -v grep | awk '{print $2}' | xargs kill
 #ps ux | grep 'sar -n DEV' | grep -v grep | awk '{print $2}' | xargs kill
-ps ux | grep 'top -p' | grep -v grep | awk '{print $2}' | xargs kill
+#ps ux | grep 'top -p' | grep -v grep | awk '{print $2}' | xargs kill
+killall pidstat
+sudo killall tcpdump
