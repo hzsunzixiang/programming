c20528334b2349d3fc9be91a6237af81c1f53fc6
hzsunzixiang <haichengsun123@163.com>
2017-09-06 14:44:11 +0800

    shuffle fisher-yates

--- random/fisher-yates/main.cpp
+++ random/fisher-yates/main.cpp
@@ -87,7 +87,7 @@ for i from 0 to n−2 do
      j ← random integer such that i ≤ j < n
      exchange a[i] and a[j]
   ***/
-int shuffle(int* arrayShuffle, size_t arraySize)
+int shuffleup(int* arrayShuffle, size_t arraySize)
 {
 	std::vector<unsigned int> randoms = _rand(arraySize - 1);
 	cout << "the random:" << endl;
@@ -108,6 +108,35 @@ int shuffle(int* arrayShuffle, size_t arraySize)
 	return 0;
 }
 
+// -- To shuffle an array a of n elements (indices 0..n-1):
+// 	for i from n−1 downto 1 do
+// 	     j ← random integer such that 0 ≤ j ≤ i
+// 	     exchange a[j] and a[i]
+int shuffledown(int* arrayShuffle, size_t arraySize)
+{
+	std::vector<unsigned int> randoms = _rand(arraySize - 1);
+	cout << "the random:" << endl;
+	std::copy(randoms.cbegin(), randoms.cend(),
+			std::ostream_iterator<unsigned int>(std::cout, " "));
+	std::cout<<std::endl;
+
+	for (uint32_t i = arraySize-1; i>=1; i--)
+	{
+		cout << "begin i " << i << endl;
+		int index = rand_range(randoms[i], 0, i);
+		std::swap(arrayShuffle[i], arrayShuffle[index]);
+		cout << "after i " << i << "\tindex\t " << index <<  endl;
+		std::copy(arrayShuffle, arrayShuffle+arraySize,
+				std::ostream_iterator<unsigned int>(std::cout, " "));
+		cout << endl;
+	}
+	return 0;
+}
+
+int shuffle(int* arrayShuffle, size_t arraySize)
+{
+	return shuffledown(arrayShuffle, arraySize);
+}
 int main()
 {
 	int arrayShuffle[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
