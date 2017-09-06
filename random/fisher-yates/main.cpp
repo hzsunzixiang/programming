e7809ade8a6a32ae2f15110747bbf53409c69d14
hzsunzixiang <haichengsun123@163.com>
2017-09-06 14:37:19 +0800

    fisher shuffler

--- random/fisher-yates/main.cpp
+++ random/fisher-yates/main.cpp
@@ -1,50 +1,128 @@
+#include<time.h>
+#include <unistd.h>
+#include <syscall.h>
+#include<stdexcept>
+#include <vector>
+#include<algorithm>
 #include <algorithm>
 #include <iostream>
 #include <iterator>
-#include <random>
-#include <vector>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 
 using namespace std;
-int cppfunciton()
+class LinuxRand
 {
-	// seed the RNG
-	std::random_device rd;
-	std::mt19937 mt(rd());
-
-	std::vector<int> elements { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
-	std::cout << "Before: ";
-	std::copy(elements.cbegin(), elements.cend(),
-			std::ostream_iterator<int>(std::cout, " "));
-
-	auto currentIndexCounter = elements.size()-1;
-	cout<<endl;
-
-	for (auto iter = elements.rbegin(); iter != elements.rend();
-			++iter, --currentIndexCounter)
-	{
-		// get int distribution with new range
-		std::uniform_int_distribution<> dis(0, currentIndexCounter);
-		const int randomIndex = dis(mt);
-
-
-		cout << "currentIndexCounter:\t" << currentIndexCounter << "\trandomIndex:\t" << randomIndex <<endl;
-		if (*iter != elements.at(randomIndex))
+	public:
+		LinuxRand()
 		{
-			std::swap(elements.at(randomIndex), *iter);
+			init();
 		}
-	}
+		uint32_t rand()
+		{
+			uint32_t r = 0;
+			read(fd, &r, sizeof(r));
+			return r;
+		}
+		std::vector<unsigned int> rand(int size)
+		{
+			unsigned int buf[size];
+			read(fd, &buf, size * sizeof(unsigned int));
+			return std::vector<unsigned int>(&buf[0], &buf[size]);
+		}
+		std::vector<unsigned int> rand(int size, unsigned int low, unsigned int high)
+		{
+			std::vector<unsigned int> ret = this->rand(size);
+			for(int i = 0; i < size; i++)
+			{
+				ret[i] = (ret[i] % (high + 1 - low)) + low;
+			}
+			return ret;
+		}
 
-	std::cout << "\nAfter: ";
-	std::copy(elements.cbegin(), elements.cend(),
-			std::ostream_iterator<int>(std::cout, " "));
+		uint32_t operator ()()
+		{
+			return this->rand();
+		}
+
+		std::vector<unsigned int> operator ()(int size)
+		{
+			return this->rand(size);
+		}
+
+		std::vector<unsigned int> operator ()(int size, unsigned int low, unsigned int high)
+		{
+			return this->rand(size, low, high);
+		}
+
+	private:
+		bool init()
+		{
+			fd = open("/dev/urandom", O_RDONLY, 0);
+			if (fd < 0)
+			{
+				fprintf(stderr, "no /dev/urandom !!!\n");
+				return false;
+			}
+			return true;
+		}
+		int fd;
+};
+
+LinuxRand _rand;
+
+
+uint32_t rand_range(unsigned rand, unsigned int l, unsigned int h)
+{
+	return (rand % (h + 1 - l)) + l;
+}
+
+/***
+https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+
+-- To shuffle an array a of n elements (indices 0..n-1):
+for i from 0 to n−2 do
+     j ← random integer such that i ≤ j < n
+     exchange a[i] and a[j]
+  ***/
+int shuffle(int* arrayShuffle, size_t arraySize)
+{
+	std::vector<unsigned int> randoms = _rand(arraySize - 1);
+	cout << "the random:" << endl;
+	std::copy(randoms.cbegin(), randoms.cend(),
+			std::ostream_iterator<unsigned int>(std::cout, " "));
 	std::cout<<std::endl;
+
+	for (uint32_t i = 0; i < arraySize-1; i++)
+	{
+		cout << "begin i " << i << endl;
+		int index = rand_range(randoms[i], i, arraySize - 1);
+		std::swap(arrayShuffle[i], arrayShuffle[index]);
+		cout << "after i " << i << "\tindex\t " << index <<  endl;
+		std::copy(arrayShuffle, arrayShuffle+arraySize,
+				std::ostream_iterator<unsigned int>(std::cout, " "));
+		cout << endl;
+	}
 	return 0;
 }
 
-
 int main()
 {
-	cppfunciton();
+	int arrayShuffle[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+	cout << "the array to be shuffle:" << endl;
+	std::copy(arrayShuffle, arrayShuffle+sizeof(arrayShuffle)/sizeof(int),
+			std::ostream_iterator<unsigned int>(std::cout, " "));
+	cout << endl;
+
+	std::cout<<std::endl;
+	shuffle(arrayShuffle, sizeof(arrayShuffle)/sizeof(int));
+
+	cout << "the array after shuffle:" << endl;
+	std::copy(arrayShuffle, arrayShuffle+sizeof(arrayShuffle)/sizeof(int),
+			std::ostream_iterator<unsigned int>(std::cout, " "));
+	cout << endl;
 	return 0;
 }
