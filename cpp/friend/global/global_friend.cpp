ebbe6d9ce34a8fd983b1dcf7f6bcd3df10fd03e5
hzsunzixiang <haichengsun123@163.com>
2017-11-29 17:59:44 +0800

    c++ friend

---
+++ cpp/friend/global/global_friend.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+
+class A
+{
+	private:
+		int a;
+	public:
+		A() {a = 0;}
+		friend void showA(A&); // global friend function
+};
+
+void showA(A& x) {
+	// Since showA() is a friend, it can access
+	// private members of A
+	std::cout << "A::a=" << x.a << endl;
+}
+
+int main()
+{
+	A a;
+	showA(a);
+	return 0;
+}
