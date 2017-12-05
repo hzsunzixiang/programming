1be2c99dbcffe5805b2ccc0217dea8e2b74bb469
hzsunzixiang <haichengsun123@163.com>
2017-12-05 17:54:29 +0800

    asm i++

--- linux/my_csapp/asm/i++/main1.c
+++ linux/my_csapp/asm/i++/main1.c
@@ -17,7 +17,7 @@ int main()
 //    0x080483cc <+1>:     mov    %esp,%ebp                                                       
 //    0x080483ce <+3>:     sub    $0x10,%esp                                                  
 //    0x080483d1 <+6>:     movl   $0xb,-0x4(%ebp)      #        -0x4(%ebp) 存放变量 i                                      
-//    0x080483d8 <+13>:    mov    -0x4(%ebp),%eax      #        变量放入寄存器                                       
+//    0x080483d8 <+13>:    mov    -0x4(%ebp),%eax      #        变量放入寄存器 后面会复制给a                                      
 //    0x080483db <+16>:    lea    0x1(%eax),%edx       #        对变量值+1  放入寄存器 edx                                      
 //    0x080483de <+19>:    mov    %edx,-0x4(%ebp)      #        把edx寄存器中的值放入变量i  -0x4(%ebp)                                      
 //    0x080483e1 <+22>:    mov    %eax,-0x8(%ebp)      #        寄存器eax中的  放入变量a                                       
