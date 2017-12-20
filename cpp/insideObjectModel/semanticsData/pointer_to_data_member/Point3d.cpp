bc2ff0bc913d96e961bcaef5678c81f6540a8735
hzsunzixiang <haichengsun123@163.com>
2017-12-20 14:27:11 +0800

    data member

--- cpp/insideObjectModel/semanticsData/pointer_to_data_member/Point3d.cpp
+++ cpp/insideObjectModel/semanticsData/pointer_to_data_member/Point3d.cpp
@@ -1,6 +1,7 @@
 
 //http://blog.csdn.net/yiranant/article/details/47344285
 #include<cmath>
+#include<cstdio>
 #include <iostream>
 using namespace std;
 class Point3d {  
@@ -12,10 +13,22 @@ class Point3d {
 };  
 int main()
 {
-	cout << "&Point3d::_x = " << &Point3d::_x << endl;  
-	cout << "&Point3d::_y = " << &Point3d::_y << endl;  
-	cout << "&Point3d::_z = " << &Point3d::_z << endl;  
-	cout << "sizeof(Point3d) = " << sizeof(Point3d) << endl; 
+	// 这样打印是错误的  必须使用printf
+	// cout << "&Point3d::_x = " << &Point3d::_x << endl;  
+	// cout << "&Point3d::_y = " << &Point3d::_y << endl;  
+	// cout << "&Point3d::_z = " << &Point3d::_z << endl;  
+	// cout << "sizeof(Point3d) = " << sizeof(Point3d) << endl; 
+
+	// One way to do that is (I'm not sure it's portable) :
+
+	// 	void TestClass::PrintMyFuncAddress(void)
+	// 	{
+	// 		void (TestClass::* ptrtofn)() = &TestClass::MyFunc;
+	// 		cout << (void*&)ptrtofn<< endl;
+	// 	}
+	printf("&Point3d::_x = %d\n",  &Point3d::_x);
+	printf("&Point3d::_y = %d\n",  &Point3d::_y);
+	printf("&Point3d::_z = %d\n",  &Point3d::_z);
 	return 0;
 }
 
