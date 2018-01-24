517f755bac1e29215292f2a988b0bceda1764d86
hzsunzixiang <haichengsun123@163.com>
2018-01-24 09:44:06 +0800

    g++ 4.9.2 不支持 模板的模板参数

--- cpp/c++template/examples_my/5chapter/templateTemplate/stack7test.cpp
+++ cpp/c++template/examples_my/5chapter/templateTemplate/stack7test.cpp
@@ -8,6 +8,7 @@
  * This software is provided "as is" without express or implied
  * warranty, and with no claim as to its suitability for any purpose.
  */
+// gcc 4.9.2不支持
 #include <iostream>
 #include <string>
 #include <cstdlib>
