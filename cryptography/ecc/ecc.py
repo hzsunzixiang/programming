94f2af0690814d5ffacfa3b5292bfd4c277bf4f3
hzsunzixiang <haichengsun123@163.com>
2018-04-02 14:53:33 +0800

    ecc

---
+++ cryptography/ecc/ecc.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding:UTF-8
+
+
+
+
+
+def addtive(x,y):
+    return [x**3-7*x, y**2]
+
+
+
+x1=-2.35
+y1=-1.86
+
+x2=-0.1
+y2=0.836
+
+x3=3.89
+y3=5.62
+
+x4=3.89
+y4=-5.62
+
+print addtive(x1,y1)
+print addtive(x2,y2)
+print addtive(x3,y3)
+print addtive(x4,y4)
+    
---
+++ cryptography/ecc/tangent.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding:UTF-8
+
+
+
+
+
+def addtive(x,y):
+    return [x**3-3*x+5, y**2]
+
+
+
+x1=2
+y1=2.65
+
+x2=-1.11
+y2=-2.64
+
+x3=-1.11
+y3=2.64
+
+
+
+print addtive(x1,y1)
+print addtive(x2,y2)
+print addtive(x3,y3)
+    
