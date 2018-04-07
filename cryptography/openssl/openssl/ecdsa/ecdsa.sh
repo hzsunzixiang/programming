030d280a1a32e6220c895ec6b979996ca39b26c8
hzsunzixiang <haichengsun123@163.com>
2018-04-07 16:06:13 +0800

    openssl ecc ecparam

--- cryptography/openssl/openssl/ecdsa/ecdsa.sh
+++ cryptography/openssl/openssl/ecdsa/ecdsa.sh
@@ -1,2 +1,5 @@
-
 openssl ecparam -param_enc explicit -conv_form uncompressed -text -noout -no_seed -name prime256v1
+
+openssl ecparam -genkey  -param_enc explicit -conv_form uncompressed -text -noout -no_seed -name prime256v1
+
+openssl ecparam -genkey  -param_enc explicit -conv_form uncompressed -text -noout -no_seed -name prime256v1 |openssl ec -text
