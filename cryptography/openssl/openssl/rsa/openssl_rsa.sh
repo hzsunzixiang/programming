f22b3e1bbbf29de79f0a7317b6b2d94339c3fcce
hzsunzixiang <haichengsun123@163.com>
2018-04-12 19:05:48 +0800

    openssl genrsa

--- cryptography/openssl/openssl/rsa/openssl_rsa.sh
+++ cryptography/openssl/openssl/rsa/openssl_rsa.sh
@@ -6,3 +6,24 @@
 #openssl smime -pk7out -in foo.sig -out foo.pk7
 openssl genrsa -aes128 -out fd.key 2048
 openssl rsa -text -in fd.key  >rsa_alg.txt
+
+
+
+#加密私钥文件，加密的密码需要输出 或者  -passout pass:123456 指定
+
+
+openssl genrsa -out genrsa.txt -aes128 -passout pass:123456 512
+openssl rsa -text -in genrsa.txt  -passout pass:123456
+
+
+# 一条命令
+openssl genrsa -aes128 -passout pass:123456 512 | openssl rsa -text -passin pass:123456
+
+
+
+
+#  导出公钥
+#ericksun@debian:~/programming/cryptography/openssl/openssl/rsa$ ssh-keygen  -y -f fd.key
+#Enter passphrase:
+#ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC81XfHlCsDetpsJlAUWXkTLmkjUo10xuRMLrEzjSNrWnS8Rt6jVlhT98bRWayle9RM9lEceCn8TstuWRKjISN2rbzUEpEifz7VRcJWklhi80xm91/BOuLnFm/G7jjZQzAWoWWdZVxyyJk6tI8lc+AMiyFsdfHpwfvE1+dSHm5P93P/cUfuPSQKc8Hdb5Si4J7FCLLF/W99FEnleA91I1qnY7AG4tdwT2RtD0dfRtPw79VjxNQ7vnC1xUscTzshUUtlOHNcefgcXVdl8fI3U1qqDvG1jDQ2YbQL1aLDGd3V0vXxVYtGDTa3mETf6A0z8fC37Mf0amDcS92HFPRv3mrL
+#
---
+++ cryptography/openssl/openssl/rsa/rsa_16bit.py
@@ -0,0 +1,12 @@
+Private-Key: (16 bit)
+modulus: 47053 (0xb7cd)
+publicExponent: 65537 (0x10001)
+privateExponent: 39653 (0x9ae5)
+prime1: 223 (0xdf)
+prime2: 211 (0xd3)
+exponent1: 137 (0x89)
+exponent2: 173 (0xad)
+coefficient: 130 (0x82)
+-----BEGIN RSA PRIVATE KEY-----
+MCYCAQACAwC3zQIDAQABAgMAmuUCAgDfAgIA0wICAIkCAgCtAgIAgg==
+-----END RSA PRIVATE KEY-----
