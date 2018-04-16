19cffebc480290202765ab43d3bbd9c3764fc36a
hzsunzixiang <haichengsun123@163.com>
2018-04-16 21:16:48 +0800

    openssl req

--- cryptography/openssl/openssl/req/req.sh
+++ cryptography/openssl/openssl/req/req.sh
@@ -1,2 +1,7 @@
  openssl req -new -sha256 -newkey rsa:2048 -nodes -keyout dev.deliciousbrains.com.key -x509 -days 365 -out dev.deliciousbrains.com.crt
  openssl  x509 -text -in dev.deliciousbrains.com.crt  -noout
+
+
+
+openssl genrsa -out key.pem 2048
+openssl req -new -key key.pem -out req.pem
