1cb8ba7f62fbd995262151d132f18bdfb4ec549c
hzsunzixiang <haichengsun123@163.com>
2018-05-03 01:35:52 +0800

    openssl run

---
+++ openssl/run.sh
@@ -0,0 +1,7 @@
+#ericksun@debian:~/programming/openssl$ export LD_LIBRARY_PATH=/home/ericksun/programming/openssl
+#ericksun@debian:~/programming/openssl$ apps/openssl version
+#OpenSSL 1.1.1-pre7-dev  xx XXX xxxx
+
+export LD_LIBRARY_PATH=/home/ericksun/programming/openssl
+apps/openssl version
+
