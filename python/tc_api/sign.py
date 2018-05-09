5ee25f31115357dbd7462ffeabb8f876fd03d962
haichengsun <haichengsun123@163.com>
2018-05-09 14:53:51 +0800

    sign python

---
+++ python/tc_api/sign.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding:UTF-8
+#coding=gbk
+
+import hashlib
+print hashlib.sha1("actid=320001130120161229020000000116&nonce_str=zxvbnm&openid=oKz050ZsuETxI3wwIKZMDbSAa24I&sp_listid=400000000320180502164001567890oKz050ZsuETxI3wwIKZMDbSAa24I&spid=4000000003&valid_time=100000&key=123456").hexdigest().upper()
