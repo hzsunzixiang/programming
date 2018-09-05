1c8159058efd38e7851f0b86f123dcc8abc98f6b
sunzixiang <haichengsun123@163.com>
2018-09-05 08:07:04 +0800

    typedef function

---
+++ cpp/c++11/function/func1.cpp
@@ -0,0 +1,44 @@
+#include <functional>
+#include <iostream>
+
+struct Foo {
+	Foo(int num) : num_(num) {}
+	void print_add(int i) const { std::cout << num_+i << '\n'; }
+	int num_;
+};
+
+void print_num(int i)
+{
+	std::cout << i << '\n';
+}
+
+void print_num_s(int i, const std::string & s)
+{
+	std::cout << i << "\t" <<  s <<'\n';
+}
+
+struct PrintNum {
+	void operator()(int i) const
+	{
+		std::cout << i << '\n';
+	}
+};
+
+typedef std::function<void (int, const std::string &)> HTTPRequestHandler;
+int main()
+{
+	// store a free function
+	std::function<void(int)> f_display = print_num;
+	HTTPRequestHandler p = print_num_s;
+
+	p(-9, "hello");
+
+    std::function<void (int, const std::string &)> p1;
+	p1 = print_num_s;
+
+	p1(9, "world");
+
+}
+
+
+
