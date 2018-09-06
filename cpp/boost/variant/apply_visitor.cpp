6e0ad816ff7583be6fee2df6c111eea6e71b4198
sunzixiang <haichengsun123@163.com>
2018-09-06 20:31:25 +0800

    variant

--- cpp/boost/variant/apply_visitor.cpp
+++ cpp/boost/variant/apply_visitor.cpp
@@ -6,6 +6,8 @@ struct output : public boost::static_visitor<>
 {
 	void operator()(double d) const { std::cout << d << '\n'; }
 	void operator()(char c) const { std::cout << c << '\n'; }
+	// 这里多加一个倒不会报错 但是少一个会报错
+	void operator()(int c) const { std::cout << c << '\n'; }
 
 	//如果注释掉 error: no match for call to ‘(const output) (std::__cxx11::basic_string<char>&)’
 	void operator()(std::string s) const { std::cout << s << '\n'; }
