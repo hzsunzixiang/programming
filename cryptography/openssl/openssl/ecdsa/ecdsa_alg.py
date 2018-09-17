0aa1009af6e4d86f8b202d9353a465987016a603
sunzixiang <haichengsun123@163.com>
2018-09-17 15:27:05 +0800

    ecdsa

--- cryptography/openssl/openssl/ecdsa/ecdsa_alg.py
+++ cryptography/openssl/openssl/ecdsa/ecdsa_alg.py
@@ -5,3 +5,24 @@
 
 # 先占个坑  后续把 签名的计算和验证 手工计算一下
 
+
+
+
+通过参考文章， 以及实际操作 构造验证ecdsa算法
+
+https://wiki.openssl.org/index.php/Command_Line_Elliptic_Curve_Operations
+
+
+
+Elliptic Curve Digital Signature Algorithm
+https://en.wikipedia.org/wiki/Elliptic_Curve_Digital_Signature_Algorithm
+
+https://onekosmos.com/blog/the-elliptic-curve-digital-signature-algorithm/
+
+
+ECDSA and DER Signatures
+
+https://github.com/libbitcoin/libbitcoin/wiki/ECDSA-and-DER-Signatures
+
+
+
