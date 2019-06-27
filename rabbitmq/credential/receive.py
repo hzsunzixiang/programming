11df9b4158752ab6895e6ee504c2b80edb7623c6
hzsunzixiang <haichengsun123@163.com>
2019-06-27 14:29:23 +0800

    channel.exchange_bind

--- rabbitmq/credential/receive.py
+++ rabbitmq/credential/receive.py
@@ -14,16 +14,18 @@ channel = connection.channel()
 channel.queue_declare(queue='hello')
 
 # 指定exchange
-channel.exchange_declare(exchange='vstation1', exchange_type='direct')
+#channel.exchange_declare(exchange='vstation1', exchange_type='direct')
+channel.exchange_declare(exchange='logs_direct', exchange_type='direct')
+
+
+# 这一句很重要，否则不生效 
+channel.queue_bind(exchange='logs_direct', queue='hello')
+
 
 print ' [*] Waiting for messages. To exit press CTRL+C'
 
 def callback(ch, method, properties, body):
     print " [x] Received %r" % (body,)
-
-#channel.basic_consume(callback,
-#                      queue='hello',
-#                      no_ack=True)
 
 channel.basic_consume('hello',
 	       	          callback,
--- rabbitmq/credential/send.py
+++ rabbitmq/credential/send.py
@@ -11,13 +11,15 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(
 			               host='localhost',
                            virtual_host="/vstation",
 						   credentials=credentials))
+
 channel = connection.channel()
 
 channel.queue_declare(queue='hello')  
 
-channel.exchange_declare(exchange='vstation1', exchange_type='direct')
+#channel.exchange_declare(exchange='vstation1', exchange_type='direct')
+channel.exchange_declare(exchange='logs_direct', exchange_type='direct')
 
-channel.basic_publish(exchange='vstation1',
+channel.basic_publish(exchange='logs_direct',
                       routing_key='hello',
                       body='Hello World!')
 print " [x] Sent 'Hello World!'"
