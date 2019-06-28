4e9778f36c72ca8e08df2fa536d91040b38cf74c
Your Name <you@example.com>
2019-06-28 13:34:58 +0800

    ha-mode

--- rabbitmq/ha-policy/receive.py
+++ rabbitmq/ha-policy/receive.py
@@ -2,31 +2,33 @@
 # -*- coding:UTF-8
 
 import pika
-import time
 
-credentials = pika.PlainCredentials('ericksun', 'ericksun')
+exchange = 'vstation'
+vhost = 'vstation'
+user =  'vstation'
+password = 'vstation'
+queue_name =  'FLOW'
+
+credentials = pika.PlainCredentials(user, password)
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
 			               host='localhost',
-                           virtual_host="/vstation",
+                           virtual_host=vhost,
 						   credentials=credentials))
+
 channel = connection.channel()
 
-result = channel.queue_declare("hello3")
+result = channel.queue_declare(queue=queue_name, durable=True,  
+                               arguments={'x-ha-policy': 'all'})
 
 queue_name = result.method.queue
 print 'queue_name:' + queue_name
 
-
 # 指定exchange
-#channel.exchange_declare(exchange='vstation1', exchange_type='direct')
-
-
-channel.exchange_declare(exchange='logs_direct8', exchange_type='direct')
-
+channel.exchange_declare(exchange=exchange, exchange_type='direct', durable=True)
 
 # 这一句很重要，否则不生效 
-channel.queue_bind(exchange='logs_direct8', queue=queue_name)
+channel.queue_bind(exchange=exchange, queue=queue_name)
 
 
 print ' [*] Waiting for messages. To exit press CTRL+C'
@@ -34,8 +36,6 @@ print ' [*] Waiting for messages. To exit press CTRL+C'
 def callback(ch, method, properties, body):
     print " [x] Received %r" % (body,)
 
-print 'begin to consume'
-#time.sleep(15)
 channel.basic_consume(queue_name,
 	       	          callback,
                       auto_ack=True)
--- rabbitmq/ha-policy/send.py
+++ rabbitmq/ha-policy/send.py
@@ -4,28 +4,34 @@
 import pika
 import time
 
+exchange = 'vstation'
+vhost = 'vstation'
+user =  'vstation'
+password = 'vstation'
+queue_name =  'FLOW'
 
-credentials = pika.PlainCredentials('ericksun', 'ericksun')
+credentials = pika.PlainCredentials(user, password)
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
 			               host='localhost',
-                           virtual_host="/vstation",
+                           virtual_host=vhost,
 						   credentials=credentials))
 
 channel = connection.channel()
 
+result = channel.queue_declare(queue=queue_name, durable=True)  
 #  rabbitmq镜像队列的设置问题。书上提到的是在queue_declare里的arguments参数中添加"x-ha-policy":"all"键值对。但是我网上查了下，目前3.0版本以上后,就不支持这种方式了。需要在rabbitmq中添加policy.这个可以开启rabbitmq的webmanage插件，然后在web页面里面方便的做配置.
 # https://niubidian.top/blog/show/56/
 # 这个参数设置应该是没有用了
-result = channel.queue_declare(queue='hello3',  
+result = channel.queue_declare(queue=queue_name, durable=True,  
                                arguments={'x-ha-policy': 'all'})
 
 queue_name = result.method.queue
 print 'queue_name:' + queue_name
 
-channel.exchange_declare(exchange='logs_direct8', exchange_type='direct')
+channel.exchange_declare(exchange=exchange, exchange_type='direct', durable=True)
 
-channel.basic_publish(exchange='logs_direct8',
+channel.basic_publish(exchange=exchange,
                       routing_key=queue_name,
                       body='Hello World!')
 print " [x] Sent 'Hello World!'"
