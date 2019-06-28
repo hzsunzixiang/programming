0c7c4b32756cd9908d9f38f40b92296666f8d36c
Your Name <you@example.com>
2019-06-28 20:50:40 +0800

    non-mirror

---
+++ rabbitmq/non-mirror/receive.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding:UTF-8
+
+import pika
+
+exchange = 'vstation'
+vhost = 'vstation'
+user =  'vstation'
+password = 'vstation'
+#queue_name =  'FLOW'
+queue_name =  'NON_MIRROR'
+
+credentials = pika.PlainCredentials(user, password)
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+			               host='localhost',
+                           virtual_host=vhost,
+						   credentials=credentials))
+
+channel = connection.channel()
+
+result = channel.queue_declare(queue=queue_name, durable=True)  
+
+queue_name = result.method.queue
+print 'queue_name:' + queue_name
+
+# 指定exchange
+channel.exchange_declare(exchange=exchange, exchange_type='direct', durable=True)
+
+# 这一句很重要，否则不生效 
+channel.queue_bind(exchange=exchange, queue=queue_name)
+
+
+print ' [*] Waiting for messages. To exit press CTRL+C'
+
+def callback(ch, method, properties, body):
+    print " [x] Received %r" % (body,)
+
+channel.basic_consume(queue_name,
+	       	          callback,
+                      auto_ack=True)
+
+channel.start_consuming()
+
+
+
