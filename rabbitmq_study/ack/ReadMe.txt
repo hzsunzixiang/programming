b5dd4e32b87775d7a1feaffef86a82b50de6695f
hzsunzixiang <haichengsun123@163.com>
2019-07-09 21:27:52 +0800

    ack of mq

---
+++ rabbitmq_study/ack/ReadMe.txt
@@ -0,0 +1,15 @@
+
+# acknowledge one or more messages
+
+    Method: Ack (80)
+
+Advanced Message Queueing Protocol
+    Type: Method (1)
+    Channel: 1
+    Length: 13
+    Class: Basic (60)
+    Method: Ack (80)
+    Arguments
+        Delivery-Tag: 2
+        .... ...0 = Multiple: False
+
---
+++ rabbitmq_study/ack/send.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding:UTF-8
+
+import pika
+import time
+
+exchange = 'vstation'
+vhost = 'vstation'
+user =  'vstation'
+password = 'vstation'
+queue_name =  'TEST_ACK'
+
+credentials = pika.PlainCredentials(user, password)
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+			               host='localhost',
+                           virtual_host=vhost,
+						   credentials=credentials))
+
+channel = connection.channel()
+
+result = channel.queue_declare(queue=queue_name, durable=True)  
+
+queue_name = result.method.queue
+print 'queue_name:' + queue_name
+
+channel.exchange_declare(exchange=exchange, exchange_type='direct', durable=True)
+
+# 这一句很重要，否则不生效 
+channel.queue_bind(exchange=exchange, queue=queue_name)
+
+# 加上这个属性才能做到真正的持久化
+#properties = pika.BasicProperties(delivery_mode=2,
+#                                  expiration='10000000')
+properties = pika.BasicProperties(delivery_mode=2)
+channel.basic_publish(exchange=exchange, routing_key=queue_name,
+                           body="Hello World!", properties=properties)
+
+print " [x] Sent 'Hello World!'"
+connection.close()  
+
+
+#sudo rabbitmqctl add_vhost /vstation
+ 
+#sudo rabbitmqctl set_permissions -p /vstation ericksun ".*" ".*" ".*"
+ 
+#sudo  rabbitmqctl list_vhosts
+
+#sudo rabbitmqctl set_permissions -p / ericksun ".*" ".*" ".*"
