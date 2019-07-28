159b90bc2466ce7e707da622436eddbdf817d904
Your Name <you@example.com>
2019-07-28 19:05:17 +0800

    SSLKEYLOGFILE

---
+++ cryptography/https-2/SSLKEYLOGFILE/run.sh
@@ -0,0 +1,3 @@
+ SSLKEYLOGFILE=./curl_ssl_keylog.rsa curl -k https://www.ericksun.com
+ SSLKEYLOGFILE=./curl_ssl_keylog.ecc curl -k https://www.ericksun.com
+ SSLKEYLOGFILE=./curl_ssl_keylog.dss curl -k https://www.ericksun.com
