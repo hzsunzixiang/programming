d47f6e3f0c1185a14bd14fc8a9fffeaa6ebda745
haichengsun <haichengsun123@163.com>
2022-01-11 06:29:26 -0800

    commit

--- erlang/my_erlang/ets/dead_lock/run.sh
+++ erlang/my_erlang/ets/dead_lock/run.sh
@@ -1,2 +1,7 @@
-gdb -p 349161   -ex "thread apply all bt" -batch
 
+#       -ex command
+#           Execute given GDB command.
+#       -batch
+#           Run in batch mode.  Exit with status 0 after processing all the command files specified with -x (and .gdbinit, if not inhibited).  Exit with
+#           nonzero status if an error occurs in executing the GDB commands in the command files.
+gdb -p 349161   -ex "thread apply all bt" -batch
