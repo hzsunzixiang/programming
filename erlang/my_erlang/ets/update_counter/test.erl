8b77e3d90b9db672d77ace5f09fd95db70b315f9
haichengsun <haichengsun123@163.com>
2022-01-16 03:54:17 -0800

    commit

--- erlang/my_erlang/ets/update_counter/test.erl
+++ erlang/my_erlang/ets/update_counter/test.erl
@@ -11,13 +11,17 @@ func2() ->
        random:seed(A1, A2, A3),
        ListRandom = [random:uniform(255) || _ <- lists:seq(1, 10)],
        Bytes = list_to_binary(ListRandom),
-       io:format("Bytes:~p~n", [Bytes]),
+       io:format("Bytes origin:~p~n", [Bytes]),
+       %io:format("Bytes:~p~n", [ets:tab2list(Table)]),
+	   % 这里 {2, 9} 表示对 第2个字段(7)累加一个数9
+	   % Bytes  表示Key
+	   % update_counter(Tab, Key, X3 :: [UpdateOp], Default) -> [Result]  opt18引入
        ets:update_counter(Table, Bytes, {2, 9}, {Bytes, 7}),
-       %io:format("result:~p~n", [ets:tab2list(Table)])
+       io:format("result:~p~n", [ets:tab2list(Table)]),
+	   timer:sleep(3000),
        'this is an end'
      end, lists:seq(1, 50000)).
 
 start() ->
      func2(),
      'this is an end'.
-
