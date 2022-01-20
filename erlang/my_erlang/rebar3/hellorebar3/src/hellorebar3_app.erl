b0a24ba6564b0edabda6d4078225c33117699816
haichengsun <haichengsun123@163.com>
2022-01-20 07:37:23 -0800

    commit

--- erlang/my_erlang/rebar3/hellorebar3/src/hellorebar3_app.erl
+++ erlang/my_erlang/rebar3/hellorebar3/src/hellorebar3_app.erl
@@ -10,7 +10,25 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
+%        Dispatch = cowboy_router:compile([
+%        		{'_', [ {"/", hello_handler, [] }]}
+%		]),
+%        {ok, _} = cowboy:start_clear(http_listener,
+%        		[{port, 8090}],
+%                #{env => #{dispatch => Dispatch}}
+%		),
+%        hellorebar3_sup:start_link().
+    Dispatch = cowboy_router:compile([
+			{'_', [{"/", hello_handler, []}]}
+	]),
+    {ok, _} = cowboy:start_clear(http_listener, 
+			[{port, 8099}],
+            #{env => #{dispatch => Dispatch}}
+			),
     hellorebar3_sup:start_link().
+    
+
+		
 
 stop(_State) ->
     ok.
