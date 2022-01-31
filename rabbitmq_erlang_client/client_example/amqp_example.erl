0ac3a19b2bb354b28b449e07326fac67e75c0b56
haichengsun <haichengsun@163.com>
2022-02-01 03:29:49 +0800

    实验成功

--- rabbitmq_erlang_client/client_example/amqp_example.erl
+++ rabbitmq_erlang_client/client_example/amqp_example.erl
@@ -38,40 +38,40 @@ test() ->
     %% Start a network connection
     RabbitParams=#amqp_params_network{host=?HOST, username=?RABBIT_USERNAME,
                       password=?RABBIT_PASSWORD, virtual_host=?VHOST, port=?PORT},
-    %{ok, Connection} = amqp_connection:start(#amqp_params_network{}),
     io:format("amqp_connection:start begin ~n"),
-    {ok, Connection} = amqp_connection:start(RabbitParams),
+    {ok, Connection} = amqp_connection:start(#amqp_params_network{}),
+    %{ok, Connection} = amqp_connection:start(RabbitParams),
     io:format("amqp_connection:start ok ~n"),
     %% Open a channel on the connection
     io:format("amqp_connection:open_channel begin ~n"),
     {ok, Channel} = amqp_connection:open_channel(Connection),
     io:format("amqp_connection:open_channel ok ~n"),
 
-    %%%%% Declare a queue
-    %%%#'queue.declare_ok'{queue = Q}
-    %%%    = amqp_channel:call(Channel, #'queue.declare'{}),
+    %% Declare a queue
+    #'queue.declare_ok'{queue = Q}
+        = amqp_channel:call(Channel, #'queue.declare'{}),
 
-    %%%%% Publish a message
-    %%%Payload = <<"foobar">>,
-    %%%%% Publish = #'basic.publish'{exchange = <<>>, routing_key = Q},
-	%%%Publish = #'basic.publish'{exchange = ?EXCHANGE, routing_key = ?QUEUE_NAME,
-    %%%                      mandatory = true},
-    %%%amqp_channel:cast(Channel, Publish, #amqp_msg{payload = Payload}),
+    %% Publish a message
+    Payload = <<"foobar">>,
+    Publish = #'basic.publish'{exchange = <<>>, routing_key = Q},
+	%%Publish = #'basic.publish'{exchange = ?EXCHANGE, routing_key = ?QUEUE_NAME,
+    %%                     mandatory = true},
+    amqp_channel:cast(Channel, Publish, #amqp_msg{payload = Payload}),
 
-    %%%%% Poll for a message
-    %%%Get = #'basic.get'{queue = Q},
-    %%%{#'basic.get_ok'{delivery_tag = Tag}, Content}
-    %%%     = amqp_channel:call(Channel, Get),
+    %% Poll for a message
+    Get = #'basic.get'{queue = Q},
+    {#'basic.get_ok'{delivery_tag = Tag}, Content}
+         = amqp_channel:call(Channel, Get),
 
-    %%%%% Do something with the message payload
-    %%%%% (some work here)
+    %% Do something with the message payload
+    %% (some work here)
 
-    %%%%% Ack the message
-    %%%amqp_channel:cast(Channel, #'basic.ack'{delivery_tag = Tag}),
+    %% Ack the message
+    amqp_channel:cast(Channel, #'basic.ack'{delivery_tag = Tag}),
 
-    %% Close the channel
-    %amqp_channel:close(Channel),
-    %% Close the connection
+    % Close the channel
+    amqp_channel:close(Channel),
+    % Close the connection
     amqp_connection:close(Connection),
 
     ok.
