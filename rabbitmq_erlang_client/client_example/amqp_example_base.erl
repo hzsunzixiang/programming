61e1bed4b68a1cf222d49f9c21a40d9f3e1156aa
haichengsun <haichengsun@163.com>
2022-02-01 23:25:26 +0800

    commit

---
+++ rabbitmq_erlang_client/client_example/amqp_example_base.erl
@@ -0,0 +1,84 @@
+-module(amqp_example_base).
+
+-include("amqp_client.hrl").
+-compile([export_all]).
+-compile(nowarn_export_all).
+
+%-define(HOST, '192.168.142.130'). 
+%-define(HOST, "192.168.142.130"). 
+-define(HOST, "127.0.0.1"). 
+-define(RABBIT_USERNAME, vstation). 
+-define(RABBIT_PASSWORD, vstation). 
+-define(VHOST, vstation). 
+-define(EXCHANGE, vstation). 
+-define(QUEUE_NAME, 'FLOW'). 
+-define(PORT, 5672). 
+% exchange = 'vstation'
+% vhost = 'vstation'
+% user =  'vstation'
+% password = 'vstation'
+% queue_name =  'FLOW'
+
+% The #amqp_params_network record sets the following default values:
+% 
+% Parameter	Default Value
+% username	guest
+% password	guest
+% virtual_host	/
+% host	localhost
+% port	5672
+% channel_max	2047
+% frame_max	0
+% heartbeat	0
+% ssl_options	none
+% auth_mechanisms	[fun amqp_auth_mechanisms:plain/3, fun amqp_auth_mechanisms:amqplain/3]
+% client_properties	[]
+
+test() ->
+    %% Start a network connection
+    RabbitParams=#amqp_params_network{host=?HOST, username=?RABBIT_USERNAME,
+                      password=?RABBIT_PASSWORD, virtual_host=?VHOST, port=?PORT},
+    io:format("amqp_connection:start begin ~n"),
+    {ok, Connection} = amqp_connection:start(#amqp_params_network{}),
+    %{ok, Connection} = amqp_connection:start(RabbitParams),
+    io:format("amqp_connection:start ok ~n"),
+    %% Open a channel on the connection
+    io:format("amqp_connection:open_channel begin ~n"),
+    {ok, Channel} = amqp_connection:open_channel(Connection),
+    io:format("amqp_connection:open_channel ok ~n"),
+
+    %% Declare a queue
+    #'queue.declare_ok'{queue = Q}
+        = amqp_channel:call(Channel, #'queue.declare'{}),
+
+    %% Publish a message
+    Payload = <<"foobar">>,
+    Publish = #'basic.publish'{exchange = <<>>, routing_key = Q},
+	%%Publish = #'basic.publish'{exchange = ?EXCHANGE, routing_key = ?QUEUE_NAME,
+    %%                     mandatory = true},
+    amqp_channel:cast(Channel, Publish, #amqp_msg{payload = Payload}),
+
+    %% Poll for a message
+    Get = #'basic.get'{queue = Q},
+    {#'basic.get_ok'{delivery_tag = Tag}, Content}
+         = amqp_channel:call(Channel, Get),
+
+    %% Do something with the message payload
+    %% (some work here)
+
+    %% Ack the message
+    amqp_channel:cast(Channel, #'basic.ack'{delivery_tag = Tag}),
+
+    % Close the channel
+    amqp_channel:close(Channel),
+    % Close the connection
+    amqp_connection:close(Connection),
+
+    ok.
+
+start_my() ->
+   test(),
+   io:format("very ok"),
+   'this is an end'.
+
+
---
+++ rabbitmq_erlang_client/software/Readme.txt
@@ -0,0 +1,1 @@
+https://github.com/rabbitmq/rabbitmq-server/releases/tag/rabbitmq_v3_5_6
