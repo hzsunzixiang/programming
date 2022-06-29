171af7a76d11f3b77b4dc0ec8ded2ec942d50ebc
haichengsun <haichengsun123@163.com>
2022-06-29 02:59:03 -0400

    passthrough

---
+++ libvirt/passthrough/find_iommu_info.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+shopt -s nullglob
+for g in $(find /sys/kernel/iommu_groups/* -maxdepth 0 -type d | sort -V); do
+    echo "IOMMU Group ${g##*/}:"
+    for d in $g/devices/*; do
+        echo -e "\t$(lspci -nns ${d##*/})"
+    done;
+done;
+
+
+#find /sys/kernel/iommu_groups/ -type l  | sort --version-sort
+#find /sys/kernel/iommu_groups/ -type l  | sort -V
+
