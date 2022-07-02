62a68b7f1a35484683df38c80610241289ab21d6
haichengsun <haichengsun123@163.com>
2022-07-02 01:59:26 -0400

    iommu passthrough

--- libvirt/passthrough/find_iommu_info.sh
+++ libvirt/passthrough/find_iommu_info.sh
@@ -3,7 +3,7 @@ shopt -s nullglob
 for g in $(find /sys/kernel/iommu_groups/* -maxdepth 0 -type d | sort -V); do
     echo "IOMMU Group ${g##*/}:"
     for d in $g/devices/*; do
-        echo -e "\t$(lspci -nns ${d##*/})"
+        echo -e "\t$(lspci -nnDs ${d##*/})"
     done;
 done;
 
--- libvirt/passthrough/set.sh
+++ libvirt/passthrough/set.sh
@@ -1,47 +1,69 @@
+#!/bin/bash
 
+#IOMMU Group 5:
+#        0000:00:11.0 PCI bridge [0604]: VMware PCI bridge [15ad:0790] (rev 02)
+#        0000:02:00.0 USB controller [0c03]: VMware USB1.1 UHCI Controller [15ad:0774]
+#        0000:02:01.0 Ethernet controller [0200]: Intel Corporation 82545EM Gigabit Ethernet Controller (Copper) [8086:100f] (rev 01)
+#        0000:02:02.0 USB controller [0c03]: VMware USB2 EHCI Controller [15ad:0770]
+#        0000:02:04.0 Ethernet controller [0200]: Intel Corporation 82545EM Gigabit Ethernet Controller (Copper) [8086:100f] (rev 01)
 
+# 把 bridge 过滤掉，不用操作
 
+echo "before unbind"
+for i in 0000:02:00.0 0000:02:01.0 0000:02:02.0 0000:02:04.0
+do
+    echo "========$i================"
+    lspci -nnDvs $i |grep Kernel
+    echo "========$i================"
+	echo
+done
 
-for i in 0000:00:11.0 0000:02:00.0 0000:02:01.0 0000:02:02.0 0000:02:04.0
+exit 0
+#echo "0000:02:04.0" > /sys/bus/pci/devices/0000\:02\:04.0/driver/unbind
+for i in 0000:02:00.0 0000:02:01.0 0000:02:02.0 0000:02:04.0
 do
     echo "$i" > /sys/bus/pci/devices/$i/driver/unbind
 done
-#echo "0000:00:11.0" > /sys/bus/pci/devices/0000\:00\:11.0/driver/unbind
-#echo "0000:02:00.0" > /sys/bus/pci/devices/0000\:02\:00.0/driver/unbind
-#echo "0000:02:01.0" > /sys/bus/pci/devices/0000\:02\:01.0/driver/unbind
-#echo "0000:02:02.0" > /sys/bus/pci/devices/0000\:02\:00.0/driver/unbind
-#echo "0000:02:04.0" > /sys/bus/pci/devices/0000\:02\:04.0/driver/unbind
 
+echo "after unbind"
+for i in 0000:02:00.0 0000:02:01.0 0000:02:02.0 0000:02:04.0
+do
+    echo "========$i================"
+    lspci -nnDvs $i |grep Kernel
+    echo "========$i================"
+	echo
+done
 
+# 这里就不再编程实现了
+echo "find the venderId:deviceId"
+for i in 0000:02:00.0 0000:02:01.0 0000:02:02.0 0000:02:04.0
+do
+    lspci -nnDvs $i |grep $i
+done
 
-for i in "15ad 0790" "15ad 0774" "8086 100f" "15ad 0770" "8086 100f"
+#0000:02:00.0 USB controller [0c03]: VMware USB1.1 UHCI Controller [15ad:0774] (prog-if 00 [UHCI])
+#0000:02:01.0 Ethernet controller [0200]: Intel Corporation 82545EM Gigabit Ethernet Controller (Copper) [8086:100f] (rev 01)
+#0000:02:02.0 USB controller [0c03]: VMware USB2 EHCI Controller [15ad:0770] (prog-if 20 [EHCI])
+#0000:02:04.0 Ethernet controller [0200]: Intel Corporation 82545EM Gigabit Ethernet Controller (Copper) [8086:100f] (rev 01)
+
+echo "find the venderId:deviceId"
+for i in 0000:02:00.0 0000:02:01.0 0000:02:02.0 0000:02:04.0
+do
+    lspci -ns $i |grep $i
+done
+#[ericksun@centos-kvm-1 passthrough]$ for i in 0000:02:00.0 0000:02:01.0 0000:02:02.0 0000:02:04.0; do     lspci -ns $i ; done
+#02:00.0 0c03: 15ad:0774
+#02:01.0 0200: 8086:100f (rev 01)
+#02:02.0 0c03: 15ad:0770
+#02:04.0 0200: 8086:100f (rev 01)
+
+#echo "15ad 0790" > /sys/bus/pci/drivers/vfio-pci/new_id
+for i in "15ad 0774" "8086 100f" "15ad 0770" "8086 100f"
 do
     echo $i
     echo $i > /sys/bus/pci/drivers/vfio-pci/new_id
 done 
 
-for i in 0000:00:11.0 0000:02:00.0 0000:02:01.0 0000:02:02.0 0000:02:04.0
-do
-    lspci -vs $i 
-done
-#echo "15ad 0790" > /sys/bus/pci/drivers/vfio-pci/new_id
-#echo "15ad 0774" > /sys/bus/pci/drivers/vfio-pci/new_id
-#echo "8086 100f" > /sys/bus/pci/drivers/vfio-pci/new_id
-#echo "15ad 0770" > /sys/bus/pci/drivers/vfio-pci/new_id
-#echo "8086 100f" > /sys/bus/pci/drivers/vfio-pci/new_id
-
-
-
-#[root@centos-kvm-1 passthrough]# lspci -ns 0000:00:11.0
-#00:11.0 0604: 15ad:0790 (rev 02)
-#[root@centos-kvm-1 passthrough]# lspci -ns 0000:02:00.0
-#02:00.0 0c03: 15ad:0774
-#[root@centos-kvm-1 passthrough]# lspci -ns 0000:02:01.0
-#02:01.0 0200: 8086:100f (rev 01)
-#[root@centos-kvm-1 passthrough]# lspci -ns 0000:02:02.0
-#02:02.0 0c03: 15ad:0770
-#[root@centos-kvm-1 passthrough]# lspci -ns 0000:02:04.0
-#02:04.0 0200: 8086:100f (rev 01)
 
 #echo "0000:02:04.0" > /sys/bus/pci/drivers/vfio-pci/bind
 #echo -n "0000:02:04.0" > /sys/bus/pci/drivers/vfio-pci/bind
