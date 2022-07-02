5b8030d5c91a48acb83176f36c893f6125f192c9
haichengsun <haichengsun123@163.com>
2022-07-02 06:24:14 -0400

    nodedev for passthrough

---
+++ libvirt/passthrough/networkcard/virsh/ReadMe.txt
@@ -0,0 +1,3 @@
+
+# virsh nodedev-list pci
+pci_0000_02_04_0
---
+++ libvirt/passthrough/networkcard/virsh/set_network.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for i in pci_0000_02_00_0 pci_0000_02_01_0 pci_0000_02_02_0 pci_0000_02_04_0
+do
+    virsh nodedev-detach $i
+done
+
+
