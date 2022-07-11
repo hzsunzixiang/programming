882ffadf1c1bb231021b857218c99596c0baec0d
haichengsun123 <haichengsun123@162.com>
2022-07-10 22:41:56 -0400

    commit

---
+++ libvirt/xml_study/virt-install-to-xml/simple-no-import.sh
@@ -0,0 +1,15 @@
+virt-install  --name=centos7-virt-install \
+	--vcpus=2 \
+	--memory=1024 \
+	--disk path=/vm/centos7hda_from_qemu  \
+	--graphics vnc,listen=0.0.0.0 \
+	--print-xml  --dry-run
+
+
+#这样运行时错误的
+
+# [root@centos-kvm-1 virt-install-to-xml]# virt-install  --name=centos7-virt-install         --vcpus=2         --memory=1024         --disk path=/vm/centos7hda_from_qemu          --graphics vnc,listen=0.0.0.0
+# ERROR
+# An install method must be specified
+# (--location URL, --cdrom CD/ISO, --pxe, --import, --boot hd|cdrom|...)
+
