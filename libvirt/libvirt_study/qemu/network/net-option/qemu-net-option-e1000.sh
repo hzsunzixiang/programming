2b734de4b8691623cef28a34e03c357de8b5d90f
haichengsun123 <haichengsun123@162.com>
2022-07-14 08:00:30 -0400

    commit

---
+++ libvirt/libvirt_study/qemu/network/net-option/qemu-net-option-e1000.sh
@@ -0,0 +1,7 @@
+/usr/libexec/qemu-kvm -name "centos_qemu_test" -machine accel=kvm -M pc -m 768 \
+-smp 2 -boot d \
+-drive file=/vm/centos7hda_from_qemu,if=virtio,index=0,media=disk,format=raw \
+-net nic,model=e1000 \
+-vnc 0.0.0.0:7 -k en-us 
+# -vga std  
+#-net nic,model=virtio, -net user \
