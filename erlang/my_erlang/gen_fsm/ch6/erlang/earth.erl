1a73dabc29b70fe5a791bea032c105f35e2492aa
sunhaicheng <haichengsun123@163.com>
2022-08-10 05:31:02 -0400

    commit

--- erlang/my_erlang/gen_fsm/ch6/erlang/earth.erl
+++ erlang/my_erlang/gen_fsm/ch6/erlang/earth.erl
@@ -1,20 +1,25 @@
 -module(earth).
--export([start/0, init/0]).
+%-export([start/0, init/0]).
+-compile(export_all).
+-compile(nowarn_export_all).
 
-start() ->
-    spawn(?MODULE, init, []).
+start_link() ->
+    {ok, spawn_link(?MODULE, init, [])}.
 
 init() ->
+    register(?MODULE, self()),
     create_earth(),
     day().
 
 day() ->
+    io:format("in day\n"),
     receive
 	eclipse -> day();
 	sunset  -> night()
     end.
 
 night() ->
+    io:format("in night\n"),
     receive
 	sunrise ->
 	    make_roosters_crow(),
@@ -23,3 +28,15 @@ night() ->
 
 create_earth() -> ok.
 make_roosters_crow() -> ok.
+
+to_night() -> ?MODULE ! sunset.
+to_day() -> ?MODULE ! sunrise.
+to_eclipse() -> ?MODULE ! eclipse.
+
+start() ->
+    {ok,Pid} = earth:start_link(),
+    io:format("local,The Pid:: ~p~n",[Pid]),
+	to_night(),
+	to_day(),
+	to_eclipse(),
+    'this is an end'.
