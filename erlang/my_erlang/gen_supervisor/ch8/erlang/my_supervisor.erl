4e432b60bc15dd7d39291969a1651e6123c41ee8
haichengsun <haichengsun123@163.com>
2022-09-04 23:58:44 -0400

    commit

--- erlang/my_erlang/gen_supervisor/ch8/erlang/my_supervisor.erl
+++ erlang/my_erlang/gen_supervisor/ch8/erlang/my_supervisor.erl
@@ -1,5 +1,5 @@
 -module(my_supervisor).
--export([start/2, init/1, stop/1]).
+-export([start/0, start/2, init/1, stop/1]).
 
 start(Name, ChildSpecList) ->
     register(Name, Pid = spawn(?MODULE, init, [ChildSpecList])),
@@ -37,3 +37,32 @@ terminate(ChildList) ->
 %%terminate([{Pid, _} | ChildList]) ->
 %%    exit(Pid, kill),
 %%    terminate(ChildList).
+start()->
+    my_supervisor:start(coffee_sup, [{coffee_fsm, start_link, []}]),
+	Pid = whereis(coffee_fsm),
+	io:format("Pid:~p~n", [Pid]),
+	%exit(Pid, kill),
+	%whereis(coffee_fsm),
+	my_supervisor:stop(coffee_sup),
+	%whereis(coffee_fsm),
+	'this is an end'.
+% 1> my_supervisor:start(coffee_sup, [{coffee_fsm, start_link, []}]).
+% {ok,<0.60.0>}
+% Machine:Rebooted Hardware
+% Display:Make Your Selection
+% 2> Pid = whereis(coffee_fsm).
+% <0.61.0>
+% 3> io:format("Pid:~p~n", [Pid]).
+% Pid:<0.61.0>
+% ok
+% 4> exit(Pid, kill).
+% Machine:Rebooted Hardware
+% true
+% Display:Make Your Selection
+% 5> whereis(coffee_fsm).
+% <0.65.0>
+% 6> my_supervisor:stop(coffee_sup).
+% stop
+% 7> whereis(coffee_fsm).
+% undefined
+
