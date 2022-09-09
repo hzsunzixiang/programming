4c81f3f7a418b3f17b99a0229f314491671bda2f
haichengsun <haichengsun@163.com>
2022-09-09 18:01:46 +0800

    commit

--- erlang/my_erlang/release/rebar3/ernie2/bsc/apps/bsc/src/frequency_sup.erl
+++ erlang/my_erlang/release/rebar3/ernie2/bsc/apps/bsc/src/frequency_sup.erl
@@ -1,16 +1,16 @@
--module(bsc_sup).
+-module(frequency_sup).
 -behaviour(supervisor).
 
 -export([start_link/0, init/1]).
 -export([stop/0]).
 
 start_link() ->
-    supervisor:start_link({local,?MODULE}, ?MODULE, []).
+    supervisor:start_link({local,?MODULE},?MODULE, []).
 
 stop() -> exit(whereis(?MODULE), shutdown).
 
 init(_) ->
-    ChildSpecList = [child(freq_overload), child(frequency), child(simple_phone_sup)],
+    ChildSpecList = [child(freq_overload), child(frequency)],
     {ok,{{rest_for_one, 2, 3600}, ChildSpecList}}.
 
 child(Module) ->
