ff20286f494c70aec396db61af3b83553cdad14f
haichengsun <haichengsun@163.com>
2022-09-15 17:06:28 +0800

    complete

--- erlang/my_erlang/rebar3/hello_server/apps/hello_server/src/hello_server_app.erl
+++ erlang/my_erlang/rebar3/hello_server/apps/hello_server/src/hello_server_app.erl
@@ -9,9 +9,31 @@
 
 -export([start/2, stop/1]).
 
-start(_StartType, _StartArgs) ->
-    hello_server_sup:start_link().
+%start(_StartType, _StartArgs) ->
+%    hello_server_sup:start_link().
 
+
+%start(_StartType, _StartArgs) ->
+%    Dispatch = cowboy_router:compile([
+%        { Host, [{Path, Handler, InitialState}]}
+%    ]),
+%    hello_server_sup:start_link().
+
+%start(_StartType, _StartArgs) ->
+%    Dispatch = cowboy_router:compile([
+%        { <<"localhost">>, [{<<"/">>, hello_handler, [] }]
+%    ]),
+%    hello_server_sup:start_link().
+start(_StartType, _StartArgs) ->
+    Dispatch = cowboy_router:compile([
+        { <<"localhost">>, [{<<"/">>, hello_handler, []}] }
+    ]),
+    {ok, _} = cowboy:start_clear(
+        ello_listener,
+        [{port, 8081}],
+        #{env => #{dispatch => Dispatch}}
+    ),
+    hello_server_sup:start_link().
 stop(_State) ->
     ok.
 
