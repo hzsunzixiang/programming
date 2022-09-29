7ba55dccabf286c585f5a5df1df02d6b8857b035
haichengsun <haichengsun123@163.com>
2022-09-29 03:36:09 -0400

    commit

--- erlang/my_erlang/gen_server/ch4/from.erl
+++ erlang/my_erlang/gen_server/ch4/from.erl
@@ -14,3 +14,16 @@ handle_call({add, Data}, From, Sum) ->
 
 add(Data, Sum) ->
     Data + Sum.
+
+
+
+%ericksun@centos7-dev:~/programming/erlang/my_erlang/gen_server/ch4 (master)$ erl
+%Erlang/OTP 25 [erts-13.0.4] [source] [64-bit] [smp:4:4] [ds:4:4:10] [async-threads:1]
+%
+%Eshell V13.0.4  (abort with ^G)
+%1> gen_server:start({local, from}, from, 0, []).
+%{ok,<0.84.0>}
+%2> gen_server:call(from, {add,10}).
+%ok
+%From:{<0.81.0>,[alias|#Ref<0.922850377.1312620545.93646>]}, Sum:10
+
