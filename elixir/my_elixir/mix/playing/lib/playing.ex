e14ba15d6ebb06b6e5ac6a1590f71308376107d5
haichengsun <haichengsun123@163.com>
2022-09-30 04:20:50 -0400

    commit

---
+++ elixir/my_elixir/mix/playing/lib/playing.ex
@@ -0,0 +1,22 @@
+defmodule Playing do
+  @moduledoc """
+  Documentation for `Playing`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Playing.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def something_new do
+    "New!"
+  end
+end
