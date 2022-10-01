d72ba3b232feb2cebfa294d2fb819ea54bd97646
haichengsun123 <haichengsun123@163.com>
2022-10-01 21:02:40 +0800

    commit

---
+++ erlang/my_erlang/gen_fsm/ch6/gen_fsm_app/apps/gen_fsm_app/src/coffee_fsm.erl
@@ -0,0 +1,103 @@
+-module(coffee_fsm).
+-behaviour(gen_fsm).
+
+%-export([start_link/0, stop/0]).
+%-export([init/1, terminate/3, handle_event/3]).
+%-export([selection/2, payment/2, remove/2]).
+%-export([americano/0, cappuccino/0, tea/0, espresso/0, 
+%         pay/1,cancel/0, cup_removed/0]). 
+-compile(export_all).
+-compile(nowarn_export_all).
+
+
+start_link() ->
+    gen_fsm:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+init([]) -> 
+    hw:reboot(),
+    hw:display("Make Your Selection", []),
+    process_flag(trap_exit, true),
+    {ok, selection, []}.
+
+%% Client Functions for Drink Selections
+tea() ->  gen_fsm:send_event(?MODULE,{selection,tea,100}).   
+espresso() ->gen_fsm:send_event(?MODULE,{selection,espresso,150}).
+americano() -> gen_fsm:send_event(?MODULE,{selection,americano,150}).   
+cappuccino() -> gen_fsm:send_event(?MODULE,{selection,cappuccino,150}).
+
+
+
+%% Client Functions for Actions
+pay(Coin)     -> gen_fsm:send_event(?MODULE,{pay, Coin}).
+cancel()      -> gen_fsm:send_event(?MODULE,cancel).
+cup_removed() -> gen_fsm:send_event(?MODULE,cup_removed).
+
+
+
+%% State: drink selection
+selection({selection, Type, Price}, _LoopData) ->
+  hw:display("Please pay:~w",[Price]),
+  {next_state, payment, {Type, Price, 0}};
+selection({pay, Coin}, LoopData) ->
+  hw:return_change(Coin),
+  {next_state, selection, LoopData};
+selection(_Other, LoopData) ->  
+  io:format("in selection _Other:: ~p~n",[_Other]),
+  {next_state, selection, LoopData}.
+
+
+payment({pay, Coin}, {Type,Price,Paid}) when Coin+Paid >= Price ->
+    NewPaid = Coin + Paid,
+    hw:display("Preparing Drink.",[]),
+    hw:return_change(NewPaid - Price),
+    hw:drop_cup(), hw:prepare(Type),
+    hw:display("Remove Drink.", []),
+    {next_state, remove, []};
+payment({pay, Coin}, {Type,Price,Paid}) when Coin+Paid < Price ->
+    NewPaid = Coin + Paid,
+    hw:display("Please pay:~w",[Price - NewPaid]),
+    {next_state, payment, {Type, Price, NewPaid}};
+payment(cancel, {_Type, _Price, Paid}) ->
+    hw:display("Make Your Selection", []),
+    hw:return_change(Paid),
+    {next_state, selection, []};
+payment(_Other, LoopData) ->
+    {next_state, payment, LoopData}.
+
+
+%% State: remove cup 
+remove(cup_removed, LoopData) ->
+    hw:display("Make Your Selection", []),
+    {next_state, selection, LoopData};
+remove({pay, Coin}, LoopData) ->
+    hw:return_change(Coin),
+    {next_state, remove, LoopData};
+remove(_Other, LoopData) ->          
+    {next_state, remove, LoopData}.
+
+
+stop() -> gen_fsm:sync_send_all_state_event(?MODULE, stop).
+
+handle_event(stop, State, LoopData) ->
+    {stop, normal, LoopData}.
+
+terminate(_Reason, payment, {_Type,_Price,Paid}) ->
+    hw:return_change(Paid);
+terminate(_Reason, _StateName, _LoopData) ->
+    ok.
+
+
+start() ->
+    {ok, Pid} = coffee_fsm:start_link(),
+	sys:trace(Pid, true),
+	coffee_fsm:cancel(),
+	coffee_fsm:tea(),
+	coffee_fsm:cancel(),
+	coffee_fsm:americano(),
+	coffee_fsm:pay(100),
+	coffee_fsm:pay(100),
+	coffee_fsm:pay(50),
+	coffee_fsm:cup_removed(),
+	'this is an end'.
+
+
--- erlang/my_erlang/gen_fsm/ch6/gen_fsm_app/apps/gen_fsm_app/src/gen_fsm_app_sup.erl
+++ erlang/my_erlang/gen_fsm/ch6/gen_fsm_app/apps/gen_fsm_app/src/gen_fsm_app_sup.erl
@@ -13,6 +13,7 @@
 
 -define(SERVER, ?MODULE).
 
+
 start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
@@ -25,11 +26,45 @@ start_link() ->
 %%                  shutdown => shutdown(), % optional
 %%                  type => worker(),       % optional
 %%                  modules => modules()}   % optional
+
+%init(_) ->
+%    ChildSpecList = [child(freq_overload), child(frequency), child(simple_phone_sup)],
+%    {ok,{{rest_for_one, 2, 3600}, ChildSpecList}}.
+
+child(Module) ->
+    {Module, {Module, start_link, []},
+     permanent, 2000, worker, [Module]}.
+
 init([]) ->
     SupFlags = #{strategy => one_for_all,
                  intensity => 0,
-                 period => 1},
-    ChildSpecs = [],
+                 period => 1}, 
+    %ChildSpecs = [],
+    ChildSpecs = [child(coffee_fsm)],                                                                                                                                                                        
     {ok, {SupFlags, ChildSpecs}}.
 
 %% internal functions
+
+
+
+%start_link() ->
+%    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+%
+%%% sup_flags() = #{strategy => strategy(),         % optional
+%%%                 intensity => non_neg_integer(), % optional
+%%%                 period => pos_integer()}        % optional
+%%% child_spec() = #{id => child_id(),       % mandatory
+%%%                  start => mfargs(),      % mandatory
+%%%                  restart => restart(),   % optional
+%%%                  shutdown => shutdown(), % optional
+%%%                  type => worker(),       % optional
+%%%                  modules => modules()}   % optional
+%init([]) ->
+%    SupFlags = #{strategy => one_for_all,
+%                 intensity => 0,
+%                 period => 1},
+%    ChildSpecs = [],
+%    {ok, {SupFlags, ChildSpecs}}.
+%
+%%% internal functions
+
