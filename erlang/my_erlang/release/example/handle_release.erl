b6ad0e2f9022685739c3e9746616d4f60f3fc96f
haichengsun <haichengsun123@163.com>
2022-10-15 07:11:42 -0400

    commit

--- erlang/my_erlang/release/example/handle_release.erl
+++ erlang/my_erlang/release/example/handle_release.erl
@@ -5,8 +5,8 @@
 -compile(nowarn_export_all).
 
 start() -> 
-    % 生成boot和script文件
+    % 生成boot和script文件, 这里不会关心beam文件
     systools:make_script("basestation", [{path, ["bsc/ebin"]}]),
-    % 生成tar包
-	systools:make_tar ("basestation",[{erts, "/usr/lib64/erlang"}, {path, ["bsc/ebin"]}, {outdir, "ernie"}]),
+    % 生成tar包, 这里需要使用beam文件
+	% systools:make_tar ("basestation",[{erts, "/usr/lib64/erlang"}, {path, ["bsc/ebin"]}, {outdir, "ernie"}]),
 	'this is an end'.
