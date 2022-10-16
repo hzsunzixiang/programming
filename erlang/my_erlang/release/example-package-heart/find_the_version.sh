9c1b40f81af9ae8960abc9eab4bc322e180215a7
haichengsun <haichengsun123@163.com>
2022-10-16 04:07:43 -0400

    commit

---
+++ erlang/my_erlang/release/example-package-heart/find_the_version.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# 直接用 escript 脚本找到
+# LIB_DIR=`./lib_dir.escipt`
+LIB_DIR=`erl -noshell -eval 'io:format(code:lib_dir()),erlang:halt()'`
+
+#echo $LIB_DIR
+
+ERTS_VERSION=$(echo $(basename `ls -d $LIB_DIR/erts-*`)|awk -F'-' '{print $2}')
+echo $ERTS_VERSION
+KERNEL_VERSION=$(echo $(basename `ls -d $LIB_DIR/kernel-*`)|awk -F'-' '{print $2}')
+echo $KERNEL_VERSION
+STDLIB_VERSION=$(echo $(basename `ls -d $LIB_DIR/stdlib-*`)|awk -F'-' '{print $2}')
+echo $STDLIB_VERSION
+SASL_VERSION=$(echo $(basename `ls -d $LIB_DIR/sasl-*`)|awk -F'-' '{print $2}')
+echo $SASL_VERSION
+
+
+sed 's/erts_version/'"$ERTS_VERSION"'/g;s/kernel_version/'"$KERNEL_VERSION"'/g; s/stdlib_version/'"$STDLIB_VERSION"'/g; s/sasl_version/'"$SASL_VERSION"'/g' basestation.rel.template > basestation.rel
+
+
+ERLANG_DIR_NAME=$(echo $(dirname $LIB_DIR))
+#echo $ERLANG_DIR_NAME
+sed 's#ERLANG_DIR_NAME#'"$ERLANG_DIR_NAME"'#g' handle_release.erl.template > handle_release.erl
+
+
+sed 's/erts_version/'"$ERTS_VERSION"'/g' start.sh.template > start.sh
+
+chmod +x start.sh
+
+RUN_DIR_NAME=`pwd`/ernie
+sed 's#RUN_DIR_NAME#'"$RUN_DIR_NAME"'#g' bsc_heart.template > bsc_heart
