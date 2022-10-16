65f8786397f817d051cfdf69ca57d6fb24d66705
haichengsun <haichengsun123@163.com>
2022-10-16 02:36:16 -0400

    commit

--- erlang/my_erlang/release/example-package/find_the_version.sh
+++ erlang/my_erlang/release/example-package/find_the_version.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 # 直接用 escript 脚本找到
 # LIB_DIR=`./lib_dir.escipt`
@@ -15,10 +16,14 @@ SASL_VERSION=$(echo $(basename `ls -d $LIB_DIR/sasl-*`)|awk -F'-' '{print $2}')
 echo $SASL_VERSION
 
 
-sed 's/erts_version/'"$ERTS_VERSION"'/g;s/kernel_version/'"$KERNEL_VERSION"'/g; s/stdlib_version/'"$STDLIB_VERSION"'/g; s/sasl_version/'"$SASL_VERSION"'/g' basestation.rel.template | tee basestation.rel
+sed 's/erts_version/'"$ERTS_VERSION"'/g;s/kernel_version/'"$KERNEL_VERSION"'/g; s/stdlib_version/'"$STDLIB_VERSION"'/g; s/sasl_version/'"$SASL_VERSION"'/g' basestation.rel.template > basestation.rel
 
 
+ERLANG_DIR_NAME=$(echo $(dirname $LIB_DIR))
+#echo $ERLANG_DIR_NAME
+sed 's#ERLANG_DIR_NAME#'"$ERLANG_DIR_NAME"'#g' handle_release.erl.template > handle_release.erl
 
-sed 's/erts_version/'"$ERTS_VERSION"'/g' start.sh.template | tee start.sh
+
+sed 's/erts_version/'"$ERTS_VERSION"'/g' start.sh.template > start.sh
 
 chmod +x start.sh
