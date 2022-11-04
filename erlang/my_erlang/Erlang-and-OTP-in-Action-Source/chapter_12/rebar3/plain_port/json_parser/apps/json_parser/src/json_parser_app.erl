276ef66ed4c21fe95df0e9eb5cd044cfb544bc8c
haichengsun <haichengsun123@163.com>
2022-11-04 05:22:55 -0400

    commit

--- erlang/my_erlang/Erlang-and-OTP-in-Action-Source/chapter_12/rebar3/plain_port/json_parser/apps/json_parser/src/json_parser_app.erl
+++ erlang/my_erlang/Erlang-and-OTP-in-Action-Source/chapter_12/rebar3/plain_port/json_parser/apps/json_parser/src/json_parser_app.erl
@@ -9,8 +9,13 @@
 
 -export([start/2, stop/1]).
 
-start(_StartType, _StartArgs) ->
-    json_parser_sup:start_link().
+start(_Type, _StartArgs) ->
+    case json_parser_sup:start_link() of
+      {ok, Pid} ->
+          {ok, Pid};
+      Other ->
+          {error, Other}
+    end.
 
 stop(_State) ->
     ok.
--- erlang/my_erlang/Erlang-and-OTP-in-Action-Source/chapter_12/rebar3/plain_port/json_parser/apps/json_parser/src/json_parser_sup.erl
+++ erlang/my_erlang/Erlang-and-OTP-in-Action-Source/chapter_12/rebar3/plain_port/json_parser/apps/json_parser/src/json_parser_sup.erl
@@ -7,8 +7,10 @@
 
 -behaviour(supervisor).
 
+%% API
 -export([start_link/0]).
 
+%% Supervisor callbacks
 -export([init/1]).
 
 -define(SERVER, ?MODULE).
@@ -16,20 +18,11 @@
 start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
-%% sup_flags() = #{strategy => strategy(),         % optional
-%%                 intensity => non_neg_integer(), % optional
-%%                 period => pos_integer()}        % optional
-%% child_spec() = #{id => child_id(),       % mandatory
-%%                  start => mfargs(),      % mandatory
-%%                  restart => restart(),   % optional
-%%                  shutdown => shutdown(), % optional
-%%                  type => worker(),       % optional
-%%                  modules => modules()}   % optional
 init([]) ->
-    SupFlags = #{strategy => one_for_all,
-                 intensity => 0,
-                 period => 1},
-    ChildSpecs = [],
-    {ok, {SupFlags, ChildSpecs}}.
+    Server = {jp_server, {jp_server, start_link, []},
+              permanent, 2000, worker, [jp_server]},
+    Children = [Server],
+    RestartStrategy = {one_for_one, 0, 1},
+    {ok, {RestartStrategy, Children}}.
 
 %% internal functions
