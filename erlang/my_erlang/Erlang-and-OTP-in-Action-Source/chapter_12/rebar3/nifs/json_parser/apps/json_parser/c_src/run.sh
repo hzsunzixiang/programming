481b2f8e1c1185361e2b75921ac217d2406cdd06
haichengsun <haichengsun123@163.com>
2022-11-04 12:45:51 -0400

    commit

--- erlang/my_erlang/Erlang-and-OTP-in-Action-Source/chapter_12/rebar3/nifs/json_parser/apps/json_parser/c_src/run.sh
+++
@@ -1,2 +0,0 @@
- gcc -o ../priv/jp_nifs.so -fpic -shared -I/home/ericksun/program/otp-25.0.4/lib/erlang/erts-13.0.4/include  -I/home/ericksun/programming/erlang/my_erlang/Erlang-and-OTP-in-Action-Source/chapter_12/yajl_install/include/ -L/home/ericksun/programming/erlang/my_erlang/Erlang-and-OTP-in-Action-Source/chapter_12//yajl_install/lib/ jp_nifs_R14.c -lyajl
-
