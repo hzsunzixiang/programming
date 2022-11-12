39818d44e0da3837b5f538e11463e0631fd30f66
haichengsun <haichengsun123@163.com>
2022-11-12 06:02:36 -0500

    return_trace

---
+++ erlang/my_erlang/debug/chapter17/trace_pattern/flag_return_trace_test.erl
@@ -0,0 +1,22 @@
+-module(flag_return_trace_test).
+-export([start/0, send/1, loop/0]).
+
+
+start() -> spawn_link(?MODULE, loop, []).
+
+send(Pid) ->
+    Pid ! {self(), ping},
+    receive pong -> pong end.
+
+loop() ->
+    receive
+	{Pid, ping} ->
+	    %spawn(crash, do_not_exist, []),
+	    Pid ! pong,
+        inner_fun(),
+	    loop()
+    end.
+
+
+inner_fun() ->
+    io:format("hello,world~n").
