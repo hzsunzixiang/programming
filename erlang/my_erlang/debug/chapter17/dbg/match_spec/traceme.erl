afb9f50e30d75d151860049b85283819b2bb2506
haichengsun <haichengsun123@163.com>
2022-11-13 04:54:59 -0500

    commit

--- erlang/my_erlang/debug/chapter17/dbg/match_spec/traceme.erl
+++ erlang/my_erlang/debug/chapter17/dbg/match_spec/traceme.erl
@@ -2,21 +2,6 @@
 -compile(export_all).
 -compile(nowarn_export_all).
 
-%-export([start/0, send/1, loop/0]).
-%
-%start() -> spawn_link(ping, loop, []).
-%
-%
-%loop() ->
-%    receive
-%	{Pid, ping} ->
-%	    spawn(crash, do_not_exist, []),
-%	    Pid ! pong,
-%        %inner_fun(),
-%	    loop()
-%    end.
-%
-
 foo(Var) ->
     io:format("in the foo ~p~n", [Var]).
 bar(Var1, Var2) ->
