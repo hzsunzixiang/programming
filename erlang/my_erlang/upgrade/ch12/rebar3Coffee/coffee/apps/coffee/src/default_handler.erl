35f5b482684b5b3124f6c24438fb2d56e89db5d3
haichengsun <haichengsun123@163.com>
2022-12-15 05:10:39 -0500

    delete rebar3Coffee

--- erlang/my_erlang/upgrade/ch12/rebar3Coffee/coffee/apps/coffee/src/default_handler.erl
+++
@@ -1,8 +0,0 @@
--module(default_handler).
--export([init/2]).
-
-init(Req, Opts) ->
-    Req2 = cowboy_req:reply(200, [ {<<"content-type">>, 
-                                    <<"text/plain">>} ], 
-                            <<"Hello world!\n">>, Req), 
-    {ok, Req2, Opts}.
