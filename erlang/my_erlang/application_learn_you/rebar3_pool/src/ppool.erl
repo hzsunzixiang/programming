af7e97946d010c3ff7daf0e05ed552db81c16674
haichengsun <haichengsun123@163.com>
2023-01-15 10:33:39 -0500

    modify

--- erlang/my_erlang/application_learn_you/rebar3_pool/src/ppool.erl
+++ erlang/my_erlang/application_learn_you/rebar3_pool/src/ppool.erl
@@ -1,14 +1,8 @@
 %%% API module for the pool
 -module(ppool).
 -behaviour(application).
--export([start/2, stop/1, start_pool/3,
+-export([start_pool/3,
          run/2, sync_queue/2, async_queue/2, stop_pool/1]).
-
-start(normal, _Args) ->
-    ppool_supersup:start_link().
-
-stop(_State) ->
-    ok.
 
 start_pool(Name, Limit, {M,F,A}) ->
     ppool_supersup:start_pool(Name, Limit, {M,F,A}).
