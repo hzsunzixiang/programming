591d98b52efdefdb9928a2a454b9b0f0f97b78f1
haichengsun <haichengsun123@163.com>
2023-01-28 00:29:52 -0500

    commit

---
+++ rabbitmq_erlang_client/rebar3/amqp_client_hello/src/amqp_info.hrl
@@ -0,0 +1,18 @@
+
+-ifndef(AMQP_INFO_HRL).
+-define(AMQP_INFO_HRL, true).
+
+-include_lib("amqp_client/include/amqp_client.hrl").
+
+-define(HOST, "192.168.142.130"). 
+% 这里必须是二进制  而且需要设置相应的权限 start_up.sh 脚本中有
+-define(RABBIT_USERNAME, <<"vstation">>).
+-define(RABBIT_PASSWORD, <<"vstation">>).
+-define(VHOST, <<"vstation">>).
+-define(EXCHANGE, <<"vstation">>). 
+-define(QUEUE_NAME, <<"FLOW">>). 
+-define(PORT, 5672). 
+
+
+
+-endif.
