83575889bfe22ae997fa887ea294d9118ecdf1b5
haichengsun <haichengsun123@163.com>
2023-03-03 09:09:55 -0500

    commit

--- erlang/my_erlang/erlang.mk/apps_dir_deps_out_project.2/rabbitmq-components.mk
+++ erlang/my_erlang/erlang.mk/apps_dir_deps_out_project.2/rabbitmq-components.mk
@@ -9,3 +9,35 @@ RABBITMQ_COMPONENTS = myproject
 
 FORCE_REBUILD = $(RABBITMQ_COMPONENTS)
 
+
+# Umbrella-specific settings.
+# --------------------------------------------------------------------
+
+# If the top-level project is a RabbitMQ component, we override
+# $(DEPS_DIR) for this project to point to the top-level's one.
+#
+# We also verify that the guessed DEPS_DIR is actually named `deps`,
+# to rule out any situation where it is a coincidence that we found a
+# `rabbitmq-components.mk` up upper directories.
+
+possible_deps_dir_1 = $(abspath ..)
+possible_deps_dir_2 = $(abspath ../../..)
+
+ifeq ($(notdir $(possible_deps_dir_1)),deps)
+ifneq ($(wildcard $(possible_deps_dir_1)/../rabbitmq-components.mk),)
+deps_dir_overriden = 1
+DEPS_DIR ?= $(possible_deps_dir_1)
+DISABLE_DISTCLEAN = 1
+endif
+endif
+
+ifeq ($(deps_dir_overriden),)
+ifeq ($(notdir $(possible_deps_dir_2)),deps)
+ifneq ($(wildcard $(possible_deps_dir_2)/../rabbitmq-components.mk),)
+deps_dir_overriden = 1
+DEPS_DIR ?= $(possible_deps_dir_2)
+DISABLE_DISTCLEAN = 1
+endif
+endif
+endif
+
