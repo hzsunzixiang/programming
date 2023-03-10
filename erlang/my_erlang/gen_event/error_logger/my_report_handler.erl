75d0368ad10350503c686014634194e248eee873
haichengsun <haichengsun123@163.com>
2023-03-10 05:00:28 -0500

    commit

--- erlang/my_erlang/gen_event/error_logger/my_report_handler.erl
+++ erlang/my_erlang/gen_event/error_logger/my_report_handler.erl
@@ -31,3 +31,19 @@ terminate(_Reason, _Type) ->
 %%tag_event(Event) ->    
 %%    {calendar:local_time(), Event}.
 
+%% ericksun@centos7-mq1:~/programming/erlang/my_erlang/gen_event/error_logger (master)$ erl -boot start_sasl
+%% Erlang/OTP 25 [erts-13.0.4] [source] [64-bit] [smp:4:4] [ds:4:4:10] [async-threads:1]
+%% 
+%% Eshell V13.0.4  (abort with ^G)
+%% 1> error_logger:add_report_handler(my_report_handler).
+%% ok
+%% 2> observer:start().
+%% ok
+%% 3> error_logger:error_report("Serious error in my module").
+%% Event:{error_report,<0.66.0>,
+%%                     {<0.89.0>,std_error,"Serious error in my module"}}, Type:[]
+%% =ERROR REPORT==== 10-Mar-2023::04:54:47.020929 ===
+%% Serious error in my module
+%% ok
+
+
