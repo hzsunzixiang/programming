784384eeec85265baa51f56826f2946e85178d13
haichengsun <haichengsun123@163.com>
2023-03-30 09:24:33 -0400

    commit

--- linux/automake/ac_search_libs/src/main.c
+++ linux/automake/ac_search_libs/src/main.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
+#include <config.h>
+
+#if HAVE_PTHREAD_H
+# include <pthread.h>
+#endif
+
 static void * print_it(void * data)
 {
 	printf("Hello from %s!\n", (const char *)data);
 	return 0;
 }
+
 int main(int argc, char * argv[])
 {
+#if HAVE_PTHREAD_H
 	pthread_t tid;
 	pthread_create(&tid, 0, print_it, argv[0]);
 	pthread_join(tid, 0);
+#else
+	print_it(argv[0]);
+#endif
 	return 0;
 }
+
+// int main(int argc, char * argv[])
+// {
+// 	pthread_t tid;
+// 	pthread_create(&tid, 0, print_it, argv[0]);
+// 	pthread_join(tid, 0);
+// 	return 0;
+// }
