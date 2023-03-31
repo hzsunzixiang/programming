87e7047b2360fb1d2e2280252c34ea64ed9c7dd7
haichengsun <haichengsun123@163.com>
2023-03-30 22:56:54 -0400

    commit

---
+++ linux/automake/ac_arg_enable/src/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <config.h>
+
+#if HAVE_PTHREAD_H
+# include <pthread.h>
+#endif
+
+static void * print_it(void * data)
+{
+	printf("Hello from %s!\n", (const char *)data);
+	return 0;
+}
+
+
+int main(int argc, char * argv[])
+{
+#if ASYNC_EXEC
+	pthread_t tid;
+	pthread_create(&tid, 0, print_it, argv[0]);
+	pthread_join(tid, 0);
+#else
+	print_it(argv[0]);
+#endif
+	return 0;
+}
+
+//int main(int argc, char * argv[])
+//{
+//#if HAVE_PTHREAD_H
+//	pthread_t tid;
+//	pthread_create(&tid, 0, print_it, argv[0]);
+//	pthread_join(tid, 0);
+//#else
+//	print_it(argv[0]);
+//#endif
+//	return 0;
+//}
+//
+// int main(int argc, char * argv[])
+// {
+// 	pthread_t tid;
+// 	pthread_create(&tid, 0, print_it, argv[0]);
+// 	pthread_join(tid, 0);
+// 	return 0;
+// }
