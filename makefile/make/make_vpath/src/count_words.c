eb37427080645e0db181d66f039aadb79b1f99ff
haichengsun <haichengsun123@163.com>
2023-04-03 02:40:02 -0400

    commit

---
+++ makefile/make/make_vpath/src/count_words.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+extern int fee_count,fie_count,foe_count,fum_count;
+extern int yylex(void);
+int result[4];
+int main()
+{	yylex();
+	counter(result);
+
+	printf("%d %d %d %d \n",result[0],result[1],result[2],result[3]);
+	/*printf("%d %d %d %d \n",fee_count,fie_count,foe_count,fum_count);*/
+	return 0;
+
+}
---
+++ makefile/make/make_vpath/src/counter.c
@@ -0,0 +1,12 @@
+#include"../include/lexer.h"
+#include"../include/counter.h"
+void counter(int counts[4])
+{
+	while( yylex() )
+		;
+	counts[0]=fee_count;
+	counts[1]=fie_count;
+	counts[2]=foe_count;
+/*	counts[3]=fum_count;*/
+
+}
