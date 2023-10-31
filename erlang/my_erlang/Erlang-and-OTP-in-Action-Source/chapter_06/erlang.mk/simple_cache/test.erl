17a394ca11cf42a543f9bfefe1caed8c5184ab12
haichengsun <haichengsun123@163.com>
2023-10-31 08:15:40 -0400

    simple cache

---
+++ erlang/my_erlang/Erlang-and-OTP-in-Action-Source/chapter_06/erlang.mk/simple_cache/test.erl
@@ -0,0 +1,14 @@
+application:ensure_started(simple_cache).
+simple_cache:insert(abc,def).
+simple_cache:insert(ab,de).
+simple_cache:insert(a,d).
+
+Function = fun(Elem) -> simple_cache:insert(Elem,Elem) end.
+
+L = [a,b,c,d,e,f,g].
+lists:foreach(Function, L).
+L1 = lists:seq(70,10,-5).
+lists:foreach(Function, L1).
+
+simple_cache:lookup(abc).
+
