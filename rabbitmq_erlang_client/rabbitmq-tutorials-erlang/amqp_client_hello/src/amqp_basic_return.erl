08b40cd0b622947c81f4dfa8de4db8f61e50e27b
ericksun <ericksun@tencent.com>
2024-08-28 05:25:29 -0400

    basic return

--- rabbitmq_erlang_client/rabbitmq-tutorials-erlang/amqp_client_hello/src/amqp_basic_return.erl
+++ rabbitmq_erlang_client/rabbitmq-tutorials-erlang/amqp_client_hello/src/amqp_basic_return.erl
@@ -1,23 +1,10 @@
 -module(amqp_basic_return).
 
-%The broker will return undeliverable messages back to the originating client. These are messages published either with the immediate or mandatory flags set. In order for the application to get notified of a return, it must register a callback process that can process #'basic.return'{} frames.
-%-include("amqp_client/include/amqp_client.hrl").
--include_lib("amqp_client/include/amqp_client.hrl").
+-include_lib("amqp_info.hrl").
 -compile([export_all]).
 -compile(nowarn_export_all).
 
--define(HOST, "192.168.142.130"). 
-
-
-% 这里必须是二进制
-% 而且需要设置相应的权限 start_up.sh 脚本中有
--define(RABBIT_USERNAME, <<"vstation">>).
--define(RABBIT_PASSWORD, <<"vstation">>).
--define(VHOST, <<"vstation">>).
-
--define(EXCHANGE, <<"vstation">>). 
--define(QUEUE_NAME, <<"FLOW">>). 
--define(PORT, 5672). 
+%The broker will return undeliverable messages back to the originating client. These are messages published either with the immediate or mandatory flags set. In order for the application to get notified of a return, it must register a callback process that can process #'basic.return'{} frames.
 
 % 连接
 connect_amqp() ->
@@ -40,14 +27,18 @@ open_channel(Connection) ->
 
 % 声明一个exchange
 declare_exchange(Channel) ->
-    Declare = #'exchange.declare'{exchange = ?EXCHANGE},
-    #'exchange.declare_ok'{} = amqp_channel:call(Channel, Declare).
+    Declare = #'exchange.declare'{exchange = ?EXCHANGE, durable = true},
+    %% Declare = #'exchange.declare'{exchange = ?EXCHANGE, type = <<"direct">>,}, %% type 默认值为 <<"direct">> 模式，一对一
+	%% -record('exchange.declare', {ticket = 0, exchange, type = <<"direct">>, passive = false, durable = false, auto_delete = false, internal = false, nowait = false, arguments = []}).
+    #'exchange.declare_ok'{} = amqp_channel:call(Channel, Declare),
+    io:format("amqp_channel:call exchange.declare ok ~n"),
+	ok.
 
 % 声明一个队列
 declare_queue(Channel) ->
     % 如果不写队列的名字，默认是这种, <<"amq.gen-tRkmLkwbpU3NxwaRMH0eAw">>
     Declare = #'queue.declare'{
-      queue = ?QUEUE_NAME,   % 这里是二进制
+      queue = ?QUEUE_NAME_CLASSIC,   % 这里是二进制
       durable = true
     },
     #'queue.declare_ok'{queue = Q} = amqp_channel:call(Channel, Declare),
@@ -60,48 +51,9 @@ binding_queue(Q, Channel)->
                             routing_key = ?EXCHANGE},
     #'queue.bind_ok'{} = amqp_channel:call(Channel, Binding).
 
-
-consumer_message(Channel, Q) ->
-    %process (`self()`),
-    #'basic.consume_ok'{consumer_tag = Tag} = amqp_channel:subscribe(Channel, #'basic.consume'{queue = Q}, self()),
-    Tag.
-
-loop(Channel, Tag) ->
-    io:format("in loop....Tag: ~p, ~n", [Tag]),
-    receive
-        %% This is the first message received
-        #'basic.consume_ok'{} ->
-            %io:format("in basic.consume_ok....~n"),
-            loop(Channel, Tag);
-
-        %% This is received when the subscription is cancelled
-        #'basic.cancel_ok'{} ->
-            io:format("in basic.cancel_ok....~n"),
-            ok;
-%
-%decode_method_fields('basic.deliver', <<F0Len:8/unsigned, F0:F0Len/binary, F1:64/unsigned, F2Bits:8, F3Len:8/unsigned, F3:F3Len/binary, F4Len:8/unsigned, F4:F4Len/binary>>) ->
-%  F2 = ((F2Bits band 1) /= 0),
-%  #'basic.deliver'{consumer_tag = F0, delivery_tag = F1, redelivered = F2, exchange = F3, routing_key = F4};
-        %% A delivery
-        %-record(amqp_msg, {props = #'P_basic'{}, payload = <<>>}).
-        {#'basic.deliver'{delivery_tag = TagRet}, Content} ->
-            io:format("in basic.deliver....~n"),
-            %% Do something with the message payload
-            %% (some work here)
-            io:format("Tag:~p, TagRet:~p, Content:~p.~n", [Tag, TagRet, Content#amqp_msg.payload]),
-            %% Ack the message  , 注意，这里是返回的 delivery_tag 不是传进来的, 不然会失败
-            amqp_channel:cast(Channel, #'basic.ack'{delivery_tag = TagRet}),
-            io:format("after ack .....~n"),
-
-            %% Loop
-            loop(Channel, Tag)
-    end.
-
-
 publish_message(Channel, Q) ->
 
     amqp_channel:register_return_handler(Channel, self()),
-
     %% Publish a message
     Payload = <<"foobar">>,
 	% Here is an example of unrouteable message handling:
