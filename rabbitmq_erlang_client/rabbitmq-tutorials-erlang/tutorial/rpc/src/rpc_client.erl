b6fbe8d7159222f6fc9e6772dbd031d0e873ff72
ericksun <ericksun@tencent.com>
2024-09-03 02:19:35 -0400

    rpc

--- rabbitmq_erlang_client/rabbitmq-tutorials-erlang/tutorial/rpc/src/rpc_client.erl
+++ rabbitmq_erlang_client/rabbitmq-tutorials-erlang/tutorial/rpc/src/rpc_client.erl
@@ -1,60 +1,58 @@
 -module(rpc_client).
+-export([start/1]).
+
 -include_lib("amqp_client/include/amqp_client.hrl").
--compile([export_all]).
--compile(nowarn_export_all).
 
--define(QUEUE_NAME, <<"rpc_queue">>).
-
-call(N) ->
-    {ok, Connection} = amqp_connection:start(#amqp_params_network{host = "localhost"}),
-    {ok, Channel} = amqp_connection:open_channel(Connection),
-
-    #'queue.declare_ok'{queue = Queue} = amqp_channel:call(Channel, #'queue.declare'{exclusive = true}),  % 这里没有 no_ack 标志位，是需要ack的
-
-    CorrelationId = integer_to_binary(erlang:unique_integer()), 
-	%Request = integer_to_list(N),
-
-    %EncodedCorrelationId = base64:encode(<<CorrelationId:64>>), % 这个是格式是透明的
-    Props = #'P_basic'{correlation_id = CorrelationId,
-                       content_type = <<"application/octet-stream">>,
-                       reply_to = Queue},
-    Publish = #'basic.publish'{exchange = <<"">>, % 默认的exchange可以绑定任何队列，所以这里没有队列名字
-                               routing_key = ?QUEUE_NAME,   % 默认和队列名相同
-                               mandatory = true},
-    %% 这里同步发送消息
-    amqp_channel:call(Channel, Publish, #amqp_msg{props = Props,
-                                                  payload = N}),
-
-    %% 这里接收
-    io:format(" [*] Waiting for rpc. To exit press CTRL+C~n"),
-
-    amqp_channel:subscribe(Channel, #'basic.consume'{queue = Queue,
-                                                     no_ack = true}, self()),
-    receive
-        #'basic.consume_ok'{} -> ok
-    end,
-    loop(Channel).
-
-loop(Channel) ->
-    receive
-        {#'basic.deliver'{routing_key = RoutingKey}, #amqp_msg{payload = Body}} ->
-            io:format(" [x] ~p:~p~n", [RoutingKey, Body]),
-            loop(Channel)
-    end.
-
-main(Argv)->
-    N = case Argv of
-            [] ->
-                <<"30">>;
-            [R] ->
-                list_to_binary(R)
-        end,
-    
-    rpc_client:call(N),
+start(Argv) ->
+    Num = case Argv of
+             [] -> 10;
+             [Arg] -> list_to_integer(Arg)
+          end,
+    io:format(" [x] Requesting fib(~p)~n", [Num]),
+    Response = call(Num),
+    io:format(" [.] Got ~p~n", [Response]),
     ok.
 
+call(Num) ->
+    {ok, Connection} =
+        amqp_connection:start(#amqp_params_network{host = "localhost"}),
+    {ok, Channel} = amqp_connection:open_channel(Connection),
+    RequestQueue = <<"rpc_queue">>,
+    CorrelationId = uuid:get_v4(),
 
-%1> rpc_client:main(["20"]).
-% [*] Waiting for rpc. To exit press CTRL+C
-% [x] <<"amq.gen-Kgi1wvhdrm6cE3eUjYPlaw">>:<<"6765">>
-%
+    amqp_channel:call(Channel, #'queue.declare'{queue = RequestQueue}),
+
+    #'queue.declare_ok'{queue = ReplyQueue} =
+        amqp_channel:call(Channel, #'queue.declare'{exclusive = true}),
+
+    Method = #'basic.consume'{queue = ReplyQueue, no_ack = true},
+    amqp_channel:subscribe(Channel, Method, self()),
+
+    amqp_channel:cast(Channel,
+                      #'basic.publish'{
+                        exchange = <<>>,
+                        routing_key = RequestQueue},
+                      #amqp_msg{
+                        payload = integer_to_binary(Num),
+                        props = #'P_basic'{
+                            reply_to = ReplyQueue,
+                            correlation_id = CorrelationId}
+                        }),
+    
+    Response = wait_for_messages(CorrelationId),
+
+    amqp_channel:close(Channel),
+    amqp_connection:close(Connection),
+
+    Response.
+
+wait_for_messages(CorrelationId) ->
+    receive
+        {#'basic.deliver'{}, #amqp_msg{payload = Body, props = Props}} ->
+            #'P_basic'{correlation_id = CorrId} = Props,
+            if CorrelationId == CorrId ->
+                binary_to_integer(Body);
+            true ->
+                -1
+            end
+    end.
--- rabbitmq_erlang_client/rabbitmq-tutorials-erlang/tutorial/rpc/src/rpc_server.erl
+++ rabbitmq_erlang_client/rabbitmq-tutorials-erlang/tutorial/rpc/src/rpc_server.erl
@@ -1,59 +1,49 @@
 -module(rpc_server).
+-export([start/0]).
+
 -include_lib("amqp_client/include/amqp_client.hrl").
--compile([export_all]).
--compile(nowarn_export_all).
 
--define(QUEUE_NAME, <<"rpc_queue">>).
-
-%% You can use pattern matching in function arguments part
-fib(0) -> 
-    0; 
-fib(1) -> 
-    1;
-fib(N) -> 
-    fib(N-1) + fib(N-2). 
-
-main() ->
+start() ->
     {ok, Connection} =
-        amqp_connection:start(#amqp_params_network{host = "localhost"}),
+        amqp_connection:start(#amqp_params_network{host = "localhost", heartbeat = 30}),
     {ok, Channel} = amqp_connection:open_channel(Connection),
 
-    amqp_channel:call(Channel, #'queue.declare'{queue = ?QUEUE_NAME,
-                                                durable = true}),
+    amqp_channel:call(Channel, #'queue.declare'{queue = <<"rpc_queue">>}),
     io:format(" [*] Waiting for messages. To exit press CTRL+C~n"),
 
     amqp_channel:call(Channel, #'basic.qos'{prefetch_count = 1}),
-    amqp_channel:subscribe(Channel, #'basic.consume'{queue = ?QUEUE_NAME}, self()),
 
-    receive
-        #'basic.consume_ok'{} -> ok
-    end,
-    loop(Channel),
-    io:format("GOOD BYE~n").
+    Method = #'basic.consume'{queue = <<"rpc_queue">>},
+    amqp_channel:subscribe(Channel, Method, self()),
+    loop(Channel).
 
 loop(Channel) ->
     receive
-        {#'basic.deliver'{delivery_tag = Tag}, #amqp_msg{props = Props, payload = Body}} ->
-            #'P_basic'{correlation_id = CorrelationId,
-                       reply_to = Q} = Props,
-            io:format("Done [x] Received ~p~n", [Body]),
+        {#'basic.deliver'{delivery_tag = DeliveryTag}, #amqp_msg{payload = Body, props = Props}} ->
+            #'P_basic'{reply_to = ReplyTo, correlation_id = CorrelationId} = Props,
             Num = binary_to_integer(Body),
+            io:format(" [.] fib(~p)~n", [Num]),
             Response = fib(Num),
-            Properties = #'P_basic'{correlation_id = CorrelationId},
-            Publish = #'basic.publish'{exchange = <<>>,
-                                       routing_key = Q,
-                                       mandatory = true},
-            amqp_channel:call(Channel, Publish, #amqp_msg{props = Properties,
-                                                          payload = integer_to_binary(Response)}),  %% 这里一定要是二进制
-            amqp_channel:cast(Channel, #'basic.ack'{delivery_tag = Tag}),  % 客户端声明的临时队列，需要ack
+
+            amqp_channel:cast(Channel,
+                    #'basic.publish'{
+                        exchange = <<>>,
+                        routing_key = ReplyTo},
+                    #amqp_msg{
+                        payload = integer_to_binary(Response),
+                        props = #'P_basic'{
+                            correlation_id = CorrelationId}
+                        }),
+
+            amqp_channel:cast(Channel,
+                    #'basic.ack'{
+                        delivery_tag = DeliveryTag
+                    }),
+
             loop(Channel)
     end.
 
-%1> rpc_server:main().
-% [*] Waiting for messages. To exit press CTRL+C
-%Done [x] Received <<"25">>
-
-
-
-
-
+fib(0) -> 0;
+fib(1) -> 1;
+fib(N) when N > 1 -> fib(N-1) + fib(N-2);
+fib(N) when N =< 0 -> -1.
