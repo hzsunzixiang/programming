1442f7585f953e75f16fd23014ed5a2a0c847e0d
ericksun <ericksun@tencent.com>
2024-09-02 23:19:42 -0400

    send

--- rabbitmq_erlang_client/rabbitmq-tutorials-erlang/tutorial/helloworld/src/send.erl
+++ rabbitmq_erlang_client/rabbitmq-tutorials-erlang/tutorial/helloworld/src/send.erl
@@ -1,13 +1,14 @@
 -module(send).
 -compile([export_all]).
 -compile(nowarn_export_all).
+%-export([start/0]).
 
 -include_lib("amqp_client/include/amqp_client.hrl").
 
 % 连接
 connect_amqp() ->
     %% Start a network connection
-    RabbitParams=#amqp_params_network{host = "localhost"},
+    RabbitParams=#amqp_params_network{},
     io:format("amqp_connection:start begin ~n"),
     {ok, Connection} = amqp_connection:start(RabbitParams),
     io:format("amqp_connection:start ok ~n"),
@@ -24,57 +25,24 @@ open_channel(Connection) ->
 % 声明一个队列
 declare_queue(Channel) ->
     % 如果不写队列的名字，默认是这种, <<"amq.gen-tRkmLkwbpU3NxwaRMH0eAw">>
-    Declare = #'queue.declare'{
-      queue = <<"hello">>   % 这里是二进制
-    },
+    Declare = #'queue.declare'{ queue = <<"hello">> },
     #'queue.declare_ok'{queue = Q} = amqp_channel:call(Channel, Declare),
-    io:format("return Q: ~p~n", [Q]),
-    io:format("amqp_channel:call queue.declare ok ~n"),
+    io:format("amqp_channel:call queue.declare ok, return Q:~p~n", [Q]),
     Q.  % 这里的返回和声明一致，如果没有声明，则是一个随机的队列
-
 publish_message(Channel, Q) ->
-    %% Publish a message
-	Publish = #'basic.publish'{exchange = <<"">>, routing_key = <<"hello">>},
-    Payload = <<"Hello World">>,
-    Msg = #amqp_msg{payload = Payload},
+	%% -record('basic.publish', {ticket = 0, exchange = <<"">>, routing_key = <<"">>, mandatory = false, immediate = false}).
+	% Here is an example of unrouteable message handling:
+	Publish = #'basic.publish'{routing_key = Q},
+    Msg = #amqp_msg{payload = <<"Hello World!">>},  % Props = #'P_basic'{delivery_mode = 2}, %% persistent message
     amqp_channel:cast(Channel, Publish, Msg),
     io:format("amqp_channel:cast basic.publish ok ~n"),
 	ok.
-
-close_channel(Channel) ->
-    % Close the channel
-    amqp_channel:close(Channel),
-    io:format("amqp_channel:close ok ~n"),
-    ok.
-
-close_connection(Connection) ->
-    % Close the connection
-    amqp_connection:close(Connection),
-    io:format("amqp_connection:close ok ~n"),
-    ok.
 
 start() ->
    Connection=send:connect_amqp(),
    Channel=send:open_channel(Connection),
    Q=send:declare_queue(Channel),
    send:publish_message(Channel, Q),
-   send:close_channel(Channel),
-   send:close_connection(Connection), 
+   ok = amqp_channel:close(Channel),
+   ok = amqp_connection:close(Connection),
    "Finish".  
-
-%%start() ->
-%%    {ok, Connection} =
-%%        amqp_connection:start(#amqp_params_network{host = "localhost", port=5672}),
-%%    {ok, Channel} = amqp_connection:open_channel(Connection),
-%%
-%%    amqp_channel:call(Channel, #'queue.declare'{queue = <<"hello">>}),
-%%
-%%    amqp_channel:cast(Channel,
-%%                      #'basic.publish'{
-%%                         exchange = <<"">>,
-%%                         routing_key = <<"hello">>},
-%%                      #amqp_msg{payload = <<"Hello World!">>}),
-%%    io:format(" [x] Sent 'Hello World!'~n"),
-%%    ok = amqp_channel:close(Channel),
-%%    ok = amqp_connection:close(Connection),
-%%    ok.
