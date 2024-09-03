17f206ec979ecb0a4252ffbf127324f6a7d5c736
ericksun <ericksun@tencent.com>
2024-09-02 23:25:40 -0400

    recv

--- rabbitmq_erlang_client/rabbitmq-tutorials-erlang/tutorial/helloworld/src/recv.erl
+++ rabbitmq_erlang_client/rabbitmq-tutorials-erlang/tutorial/helloworld/src/recv.erl
@@ -1,19 +1,19 @@
 -module(recv).
--export([start/0]).
-%-compile([export_all]).
-%-compile(nowarn_export_all).
+-compile([export_all]).
+-compile(nowarn_export_all).
 
 -include_lib("amqp_client/include/amqp_client.hrl").
 
 start() ->
     {ok, Connection} =
-        amqp_connection:start(#amqp_params_network{host = "localhost", heartbeat = 30}),
+        amqp_connection:start(#amqp_params_network{}),
     {ok, Channel} = amqp_connection:open_channel(Connection),
 
     amqp_channel:call(Channel, #'queue.declare'{queue = <<"hello">>}),
     io:format(" [*] Waiting for messages. To exit press CTRL+C~n"),
 
     Method = #'basic.consume'{queue = <<"hello">>, no_ack = true},
+	%-record('basic.consume', {ticket = 0, queue = <<"">>, consumer_tag = <<"">>, no_local = false, no_ack = false, exclusive = false, nowait = false, arguments = []}).
     amqp_channel:subscribe(Channel, Method, self()),
     loop(Channel).
 
