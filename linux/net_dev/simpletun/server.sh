973b0f13d88763ec72676f7d7a066419644eb128
ericksun <ericksun@tencent.com>
2024-09-21 07:39:20 -0400

    commit

--- linux/net_dev/simpletun/server.sh
+++ linux/net_dev/simpletun/server.sh
@@ -5,9 +5,9 @@
 
 
 #ip tuntap add dev tun3 mode tap user ericksun
-#ip tuntap del dev tun3 mode tun 
-ip tuntap add dev tun3 mode tun
-ip addr add 192.168.0.2/24 dev tun3
-ip link set tun3 up
+#sudo ip tuntap del dev tun3 mode tun 
+sudo ip tuntap add dev tun3 mode tun
+sudo ip addr add 192.168.0.2/24 dev tun3
+sudo ip link set tun3 up
 
 ./simpletun -i tun3 -s
