298b7cac88db479191be3607e1c435fa0cd3512d
haichengsun <haichengsun123@163.com>
2024-11-06 12:05:05 +0800

    commit

--- strongswan/certificate/create_ca.sh
+++ strongswan/certificate/create_ca.sh
@@ -5,15 +5,18 @@ ca_file=strongswanCert.pem
 swanctl_dir=/etc/strongswan/swanctl/
 swanctl_conf_dir=$swanctl_dir/conf.d/
 
-#private_key_file_for_end_ca=moonKey.pem
-#end_ca_request_file=moonReq.pem
-#end_ca_file=moonCert.pem
-#domain_name_for_ca=moon.strongswan.org
+private_key_file_for_end_ca=moonKey.pem
+end_ca_request_file=moonReq.pem
+end_ca_file=moonCert.pem
+domain_name_for_ca=moon.strongswan.org
 
-private_key_file_for_end_ca=sunKey.pem
-end_ca_request_file=sunReq.pem
-end_ca_file=sunCert.pem
-domain_name_for_ca=sun.strongswan.org
+key_type=ecdsa  #ed25519
+key_length=521
+
+#private_key_file_for_end_ca=sunKey.pem
+#end_ca_request_file=sunReq.pem
+#end_ca_file=sunCert.pem
+#domain_name_for_ca=sun.strongswan.org
 
 
 #################################
@@ -23,13 +26,14 @@ function generate_ca_file()
 {
     # 生成密钥（128位的elliptic Edwards-Curve的key）
     # generates an elliptic Edwards-Curve key with a cryptographic strength of 128 bits. Alternative key types are ecdsa, classic rsa and ed448.
-    pki --gen --type ed25519 --outform pem > $private_key_file_for_ca
+    pki --gen  --size $key_length --type $key_type --outform pem > $private_key_file_for_ca
     #对应的公钥被打包成一个自签名的CA证书，有效期为10年（3652天）：
     #根据秘钥生成证书 用上述生成的秘钥中的公钥：$private_key_file_for_ca,打包成一个自签名的CA证书
     pki --self --ca --lifetime 3652 --in $private_key_file_for_ca \
                --dn "C=CH, O=strongSwan, CN=strongSwan Root CA" \
                --outform pem > $ca_file
 }
+
 
 #################################
 ######## 生成终端实体证书 ###########
@@ -38,7 +42,7 @@ function generate_end_ca_file()
 {
     #生成终端实体证书
     #使用以下命令为host 生成Ed25519私钥：
-    pki --gen --type ed25519 --outform pem > $private_key_file_for_end_ca
+    pki --gen --size $key_length --type $key_type --outform pem > $private_key_file_for_end_ca
     
     
     #接下来，使用以下命令创建PKCS#10证书请求：
@@ -53,43 +57,6 @@ function generate_end_ca_file()
         --outform pem > $end_ca_file
 }
 
-
-
-#################################
-######## 生成  CA证书 ###########
-#################################
-function generate_ca_file()
-{
-    # 生成密钥（128位的elliptic Edwards-Curve的key）
-    # generates an elliptic Edwards-Curve key with a cryptographic strength of 128 bits. Alternative key types are ecdsa, classic rsa and ed448.
-    pki --gen --type ed25519 --outform pem > $private_key_file_for_ca
-    #对应的公钥被打包成一个自签名的CA证书，有效期为10年（3652天）：
-    #根据秘钥生成证书 用上述生成的秘钥中的公钥：$private_key_file_for_ca,打包成一个自签名的CA证书
-    pki --self --ca --lifetime 3652 --in $private_key_file_for_ca \
-               --dn "C=CH, O=strongSwan, CN=strongSwan Root CA" \
-               --outform pem > $ca_file
-}
-
-#################################
-######## 生成终端实体证书 ###########
-#################################
-function generate_end_ca_file()
-{
-    #生成终端实体证书
-    #使用以下命令为host 生成Ed25519私钥：
-    pki --gen --type ed25519 --outform pem > $private_key_file_for_end_ca
-    
-    #接下来，使用以下命令创建PKCS#10证书请求：
-    pki --req --type priv --in $private_key_file_for_end_ca \
-        --dn "C=CH, O=strongSwan, CN=$domain_name_for_ca" \
-        --san $domain_name_for_ca --outform pem > $end_ca_request_file
-    
-    #基于证书请求，CA使用以下命令签发签名的终端实体证书：
-    
-    pki --issue --cacert $ca_file --cakey $private_key_file_for_ca \
-        --type pkcs10 --in $end_ca_request_file --serial 01 --lifetime 1826 \
-        --outform pem > $end_ca_file
-}
 
 function put_file_to_directory()
 {
