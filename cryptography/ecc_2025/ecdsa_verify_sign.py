b74efa8bda0df222bd5ea565ac6c33edd5b821de
hzsunzixiang <haichengsun123@163.com>
2025-03-02 23:15:46 +0800

    ecc sign

--- cryptography/ecc_2025/ecdsa_verify_sign.py
+++ cryptography/ecc_2025/ecdsa_verify_sign.py
@@ -17,57 +17,12 @@ def modinv(a, m):
     	raise ValueError
     return x % m
 
-# 这是参数 Prime:
-p=2**256 - 2**32 - 977
-a=0
-b=7
-
-# 这个是生成元
-x1=0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798
-y1=0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8
-
-# 这个是公钥
-x2=0xb9c7077daaa55acf00048bca3c5d04d053a5a4e48c32c88e6776ccc275c94daf
-y2=0x473a9dbea3fb3ebcb43797a9583c634fd205c7d87469b0e4f2a5d3f6a794f424
-
-
-
-# 首先验证这2个点
-print ((x1**3+a*x1+b)%p == (y1**2)%p)
-print ((x2**3+a*x2+b)%p == (y2**2)%p)
-
-
-# 这是私钥
-# 验证公式为 (x2,y2) = priv(x1,y1)
-priv = 0xea628ae24b0a6852fc55f8a40bf07fa9a9e3be78d76359671f2c4aa2e695ca20
-
-
-# https://stackoverflow.com/questions/37377982/remove-the-0b-in-binary
-# https://pythonspot.com/binary-numbers-and-logical-operators/
-# https://stackoverflow.com/questions/1476/how-do-you-express-binary-literals-in-python
-# https://en.wikipedia.org/wiki/Elliptic_curve_point_multiplication
-
-#print str(1)+"p:\t", (x1,y1)
-# 必须得用算法才可以 不能这么蛮力
-#  Q ← 0
-#   for j ← i − 1 downto 0 do
-#       Q ← point_double(Q)
-#       if (dj != 0)
-#           Q ← point_add(Q, djG)
-#   return Q
-
-#for i in range(2,priv):
-
-
-
 # 然后用 算法实现
-#i=0
 def ecc_double(x1,y1):
     s=((3*(x1**2)+a)*modinv(2*y1,p))%p
     x3=(s**2-x1-x1)%p
     y3=(s*(x1-x3)-y1)%p
     return (x3,y3)
-#print ecc_double(x1,y1)
 
 def ecc_add(x1,y1,x2,y2):
     s = 0
@@ -96,37 +51,11 @@ def ecc_cal(x1,y1,priv):
            (x_tmp, y_tmp) = (x3,y3)
     return (x3, y3)
 
-(x3, y3)=ecc_cal(x1, y1, priv)
-print ("result---:",(hex(x3),hex(y3)))
-print ("known---:",(hex(x2),hex(y2)))
-print ((x3,y3)==(x2,y2))
-
-#Order: 
-#    00:ff:ff:ff:ff:ff:ff:ff:ff:ff:ff:ff:ff:ff:ff:
-#    ff:fe:ba:ae:dc:e6:af:48:a0:3b:bf:d2:5e:8c:d0:
-#    36:41:41
-
-#这个即为素数阶 q
-q=0x00fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
-
-#点A = (xa=0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798, ya=0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8)
-# 这个是生成元 就是上面的(x1,y1)
-xa=0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798
-ya=0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8
-
-
-# 这个是公钥
-xb=0xb9c7077daaa55acf00048bca3c5d04d053a5a4e48c32c88e6776ccc275c94daf
-yb=0x473a9dbea3fb3ebcb43797a9583c634fd205c7d87469b0e4f2a5d3f6a794f424
-
-
-# 对 hashx进行签名
-hashx=0x8d8351fcd1f284e5f22e4c60886911cc104fc302bd5debe4ea6bfbea1cd34684
 
 #(p,a,b,q,A,B)   B为公钥
 # (d)为私钥
 # 曲线的p a b 上面已经给出
-def signature(priv, hashx): 
+def signature(priv, hashx, xa, ya): 
     # 选择d的值 , d即为私钥
     d = priv 
     # 计算B=dA B即为公钥
@@ -149,11 +78,8 @@ def signature(priv, hashx):
     s=((hashx+d*r)*kE_reverse)%q
     return (r,s)
 
-(r,s) = signature(priv, hashx)
-print((hex(r), hex(s)))
 
-
-def verify_sign(r, s, hashx):
+def verify_sign(r, s, hashx, q, xa, ya):
     s_reverse=modinv(s, q)
     w=s_reverse%q
     u1=(w*hashx)%q
@@ -163,13 +89,34 @@ def verify_sign(r, s, hashx):
     (u2B_x, u2B_y)=ecc_cal(xb,yb,u2) 
     (xp, yp) = ecc_add(u1A_x,u1A_y,u2B_x,u2B_y)
     return (xp, yp)
-
-(xp,yp)=verify_sign(r,s,hashx)
-print("xp:---", hex(xp))
-print("r:---", hex(r))
-
-if xp==r:
-    print("success")
-else:
-    print("failed")
-
+if __name__ == '__main__':
+    # 这是参数 Prime:
+    p=2**256 - 2**32 - 977
+    a=0
+    b=7
+    # 这是私钥
+    priv = 0xea628ae24b0a6852fc55f8a40bf07fa9a9e3be78d76359671f2c4aa2e695ca20
+    #这个即为素数阶 q
+    q=0x00fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
+    # 这个是生成元 
+    xa=0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798
+    ya=0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8
+    # 这个是公钥
+    xb=0xb9c7077daaa55acf00048bca3c5d04d053a5a4e48c32c88e6776ccc275c94daf
+    yb=0x473a9dbea3fb3ebcb43797a9583c634fd205c7d87469b0e4f2a5d3f6a794f424
+    # 对 hashx进行签名
+    hashx=0x8d8351fcd1f284e5f22e4c60886911cc104fc302bd5debe4ea6bfbea1cd34684
+    
+    # 用私钥 签名
+    (r,s) = signature(priv, hashx, xa, ya)
+    print((hex(r), hex(s)))
+    
+    (xp,yp)=verify_sign(r,s,hashx, q, xa, ya)
+    print("xp:---", hex(xp))
+    print("r:---", hex(r))
+    
+    if xp==r:
+        print("success")
+    else:
+        print("failed")
+    
