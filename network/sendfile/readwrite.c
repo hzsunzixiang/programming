3045834e7488988585ef924c5b25076ec1ca9163
ericksun <ericksun@tencent.com>
2025-05-21 02:56:36 -0400

    sendfile test on local file

---
+++ network/sendfile/readwrite.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#define BUF_SIZE 4096*1000
+int main(int argc, char **argv) {
+    char buf[BUF_SIZE];
+    //const char *fromfile = argv[1];
+    //const char *tofile = argv[2];
+    const char *fromfile = "Makefile";
+    const char *tofile = "Makefile_copy";
+    struct stat stat_buf;
+    int fromfd = open(fromfile, O_RDONLY);
+    fstat(fromfd, &stat_buf);
+    int tofd = open(tofile, O_WRONLY | O_CREAT, stat_buf.st_mode);
+    int n;
+    while ((n = read(fromfd, &buf, sizeof(buf))) > 0) {
+        write(tofd, &buf, n);
+    }
+}
---
+++ network/sendfile/sendfile.c
@@ -0,0 +1,19 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/sendfile.h>
+#define BUF_SIZE 4096*1000
+int main(int argc, char **argv) {
+    //const char *fromfile = argv[1];
+    //const char *tofile = argv[2];
+    const char *fromfile = "Makefile";
+    const char *tofile = "Makefile_copy";
+    struct stat stat_buf;
+    int fromfd = open(fromfile, O_RDONLY);
+    fstat(fromfd, &stat_buf);
+    int tofd = open(tofile, O_WRONLY | O_CREAT, stat_buf.st_mode);
+    int n = 1;
+    while (n > 0) {
+        n = sendfile(tofd, fromfd, 0, BUF_SIZE);
+    }
+}
